618511da7aacc43ebb8a01eac9e9521051377c12
Buster Neece <buster@busterneece.com>
2014-06-06 10:59:54 -0500

    Initial work on song profile page.

--- app/modules/default/controllers/SongController.php
+++ app/modules/default/controllers/SongController.php
@@ -7,7 +7,34 @@ class SongController extends \DF\Controller\Action
 {
     public function indexAction()
     {
+        $id = (int)$this->getParam('id');
+        $record = Song::find($id);
 
+        if (!($record instanceof Song))
+            throw new \DF\Exception\DisplayOnly('Song not found!');
+
+        $song_info = array();
+        $song_info['record'] = $record;
+
+        // Get external provider information.
+        $song_info['external'] = $record->getExternal();
+
+        // Temporary replacement for locally cached song art.
+        if ($song_info['external']['bronytunes']['image_url'])
+            $song_info['image_url'] = $song_info['external']['bronytunes']['image_url'];
+        elseif ($song_info['external']['eqbeats']['image_url'])
+            $song_info['image_url'] = $song_info['external']['eqbeats']['image_url'];
+        elseif ($song_info['external']['ponyfm']['image_url'])
+            $song_info['image_url'] = $song_info['external']['ponyfm']['image_url'];
+        else
+            $song_info['image_url'] = \DF\Url::content('images/song_generic.png');
+
+        // Get most recent playback information.
+        
+
+        // Get requestable locations.
+
+        $this->view->song = $song_info;
     }
 
     /**

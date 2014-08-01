337f90ef267e306c5e9b4bcf31479665b9398b90
Buster Neece <buster@busterneece.com>
2014-08-01 06:33:36 -0500

    Update header script to improve SEO support.

--- web/static/header/script.js
+++ web/static/header/script.js
@@ -1,6 +1,11 @@
 /**
  * Ponyville Live! Header Banner Include
  * Requires jQuery 1.8+
+ *
+ * Usage:
+ * Immediately after <body> tag, add:
+ * <a id="pvl_header_link" href="http://www.ponyvillelive.com/" target="_blank">Ponyville Live!</a>
+ * <script type="text/javascript" src="http://www.ponyvillelive.com/static/header/script.js"></script>
  */
 
 if (jQuery) {
@@ -10,7 +15,16 @@ if (jQuery) {
 		// Load supporting CSS.
 		$("head").append("<link rel='stylesheet' href='//static.ponyvillelive.com/header/header.css' type='text/css' media='screen'>");
 
-		$("body").prepend('<div class="pvl-navbar" style="height: 42px; overflow: hidden;"><div class="pvl-navbar-inner"><div class="pvl-container"><a href="http://www.ponyvillelive.com/" target="_blank"><img src="//static.ponyvillelive.com/header/header_dark.png" alt="This station is a proud partner of Ponyville Live! - Ponyville Live, Bringing Pony People Together."></div></div></div>');
+        // Move existing link to top of body if detected, or recreate.
+        if ($('#pvl_header_link').length > 0)
+            $(document.body).prepend($('#pvl_header_link').detach());
+        else
+            $(document.body).prepend('<a id="pvl_header_link" href="http://www.ponyvillelive.com/" target="_blank">Ponyville Live!</a>');
 
-	})(jQuery)
+        var header_link = $('#pvl_header_link');
+        header_link.wrap('<div class="pvl-navbar-inner" />').wrap('<div class="pvl-navbar" />');
+        header_link.html('<img src="//ponyvillelive.com/static/header/header_dark.png" alt="This station is a proud partner of Ponyville Live! - Ponyville Live, Bringing Pony People Together.">');
+
+	})(jQuery);
+
 }

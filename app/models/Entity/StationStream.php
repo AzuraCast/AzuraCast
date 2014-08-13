37747e9c3eacde3cd75f164c5785103e20dbdc3f
Buster Neece <buster@busterneece.com>
2014-08-13 01:18:22 -0500

    Create new StationStream entity.

---
+++ app/models/Entity/StationStream.php
@@ -0,0 +1,45 @@
+<?php
+namespace Entity;
+
+use \Doctrine\Common\Collections\ArrayCollection;
+
+/**
+ * @Table(name="station_streams")
+ * @Entity
+ */
+class StationStream extends \DF\Doctrine\Entity
+{
+    public function __construct()
+    {
+        $this->is_default = 0;
+    }
+
+    /**
+     * @Column(name="id", type="integer")
+     * @Id
+     * @GeneratedValue(strategy="IDENTITY")
+     */
+    protected $id;
+
+    /** @Column(name="station_id", type="integer") */
+    protected $station_id;
+
+    /** @Column(name="is_default", type="boolean") */
+    protected $is_default;
+
+    /** @Column(name="name", type="string", length=75, nullable=true) */
+    protected $name;
+
+    /** @Column(name="type", type="string", length=50, nullable=true) */
+    protected $type;
+
+    /** @Column(name="listen_url", type="string", length=150, nullable=true) */
+    protected $listen_url;
+
+    /** @Column(name="nowplaying_url", type="string", length=100, nullable=true) */
+    protected $nowplaying_url;
+
+    /** @Column(name="nowplaying_data", type="json", nullable=true) */
+    protected $nowplaying_data;
+
+}

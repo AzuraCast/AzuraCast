71b630851c7fef3772ed4ff1551f94c1642d9df4
Buster Silver <buster@busterneece.com>
2015-03-08 05:15:43 -0500

    Deduplication script, preparing for new DB schema changes for external songs.

--- app/modules/frontend/controllers/UtilController.php
+++ app/modules/frontend/controllers/UtilController.php
@@ -18,6 +18,47 @@ class UtilController extends BaseController
 
         \PVL\Debug::setEchoMode();
 
+        $em = $this->di->get('em');
+        $tables = array('SongExternalBronyTunes', 'SongExternalEqBeats', 'SongExternalPonyFm');
+
+        foreach($tables as $table)
+        {
+            $i = 0;
+            $hashes_seen = array();
+
+            $q = $em->createQuery('select se FROM Entity\\'.$table.' se ORDER BY se.id ASC');
+
+            $iterableResult = $q->iterate();
+            foreach ($iterableResult as $row)
+            {
+                $ext_row = $row[0];
+                $hash = $ext_row->hash;
+
+                if (isset($hashes_seen[$hash]))
+                {
+                    $em->remove($ext_row);
+                }
+                else
+                {
+                    $hashes_seen[$hash] = $hash;
+                }
+
+                ++$i;
+
+                if (($i % 100) === 0)
+                {
+                    $em->flush(); // Executes all updates.
+                    $em->clear(); // Detaches all objects from Doctrine!
+                }
+            }
+        }
+
+        $em->flush(); // Executes all updates.
+        $em->clear(); // Detaches all objects from Doctrine!
+
+        echo 'Duplicates removed.';
+        exit;
+
         // Sync the BronyTunes library.
         \PVL\Service\BronyTunes::load(true);
 

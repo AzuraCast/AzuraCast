7501c8290b9a2474c8c263070eb0417766c2a83e
Buster Silver <buster@busterneece.com>
2015-04-19 04:52:49 -0500

    First push of InfluxDB configuration!

---
+++ app/config/influx.conf.sample.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * InfluxDB Configuration
+ */
+
+return array(
+    'host'      => 'localhost',
+    'port'      => 8086,
+    'username'  => 'root',
+    'password'  => 'root',
+    'protocol'  => 'http',
+);

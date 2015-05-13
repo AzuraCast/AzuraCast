b8cf844ccbc72d26938f504fb7a7fbb68fe39bec
Buster Silver <buster@busterneece.com>
2015-05-12 23:55:04 -0500

    Clean up test pages.

--- app/modules/frontend/controllers/UtilController.php
+++ app/modules/frontend/controllers/UtilController.php
@@ -19,12 +19,6 @@ class UtilController extends BaseController
         ini_set('memory_limit', '-1');
 
         Debug::setEchoMode();
-
-        $all_keys = \DF\Cache::getKeys();
-        Debug::print_r($all_keys);
-
-        Debug::log(\DF\Cache::getSitePrefix());
-
         Debug::log('Donezo!');
     }
 }

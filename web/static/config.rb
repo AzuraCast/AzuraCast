1b5dd603a967b2cb7bc566b5faaea5fe1f7dfa99
Buster Silver <buster@busterneece.com>
2015-08-09 19:26:18 -0500

    Some updates to the SCSS handling to support Koala (a very, very kewl app).

---
+++ web/static/config.rb
@@ -0,0 +1,10 @@
+preferred_syntax = :sass
+http_path = '../'
+css_dir = 'compiled'
+sass_dir = 'sass'
+images_dir = 'images'
+javascripts_dir = 'js'
+relative_assets = true
+sourcemap = true
+
+output_style = :compressed

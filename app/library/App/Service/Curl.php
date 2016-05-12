b59768077ff38709d602a20500c5ea48c2eb1e5f
Buster Silver <loobalightdark@gmail.com>
2016-05-12 06:16:10 -0500

    Fix basic "now playing" synchronization task running.

--- app/library/App/Service/Curl.php
+++ app/library/App/Service/Curl.php
@@ -1,8 +1,8 @@
 <?php
 namespace App\Service;
 
-use PVL\Debug;
-use PVL\Utilities;
+use App\Debug;
+use App\Utilities;
 
 class Curl
 {
@@ -69,7 +69,7 @@ class Curl
         if (!empty($c_opts['headers']))
             curl_setopt($curl, CURLOPT_HTTPHEADER, $c_opts['headers']);
 
-        $return_raw = Utilities::curl_exec_utf8($curl);
+        $return_raw = self::curl_exec_utf8($curl);
         // End cURL request.
 
         Debug::endTimer('Make cURL Request');
@@ -90,5 +90,52 @@ class Curl
             Debug::log("Curl error: ".$error);
 
         return trim($return_raw);
+    }
+
+    public static function curl_exec_utf8($ch)
+    {
+        $data = curl_exec($ch);
+        if (!is_string($data)) return $data;
+
+        unset($charset);
+        $content_type = curl_getinfo($ch, CURLINFO_CONTENT_TYPE);
+
+        /* 1: HTTP Content-Type: header */
+        preg_match( '@([\w/+]+)(;\s*charset=(\S+))?@i', $content_type, $matches );
+        if ( isset( $matches[3] ) )
+            $charset = $matches[3];
+
+        /* 2: <meta> element in the page */
+        if (!isset($charset)) {
+            preg_match( '@<meta\s+http-equiv="Content-Type"\s+content="([\w/]+)(;\s*charset=([^\s"]+))?@i', $data, $matches );
+            if ( isset( $matches[3] ) )
+                $charset = $matches[3];
+        }
+
+        /* 3: <xml> element in the page */
+        if (!isset($charset)) {
+            preg_match( '@<\?xml.+encoding="([^\s"]+)@si', $data, $matches );
+            if ( isset( $matches[1] ) )
+                $charset = $matches[1];
+        }
+
+        /* 4: PHP's heuristic detection */
+        if (!isset($charset)) {
+            $encoding = mb_detect_encoding($data);
+            if ($encoding)
+                $charset = $encoding;
+        }
+
+        /* 5: Default for HTML */
+        if (!isset($charset)) {
+            if (strstr($content_type, "text/html") === 0)
+                $charset = "ISO 8859-1";
+        }
+
+        /* Convert it if it is anything but UTF-8 */
+        if (isset($charset) && strtoupper($charset) != "UTF-8")
+            $data = iconv($charset, 'UTF-8//IGNORE', $data);
+
+        return $data;
     }
 }

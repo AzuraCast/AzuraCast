856770bb7e8888e8600a441e1b434a71e1cfd5fb
Buster Silver <loobalightdark@gmail.com>
2016-09-10 23:33:30 -0500

    Closes #27 - Optimize the update process.

--- update.sh
+++ update.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
-sudo apt-get update
-sudo apt-get install -q -y software-properties-common
-sudo apt-add-repository ppa:ansible/ansible
-sudo apt-get update
-sudo apt-get install -q -y ansible python-mysqldb
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' ansible|grep "install ok installed")
+echo Checking for Ansible: $PKG_OK
+
+if [ "" == "$PKG_OK" ]; then
+    sudo apt-get update
+    sudo apt-get install -q -y software-properties-common
+    sudo apt-add-repository ppa:ansible/ansible
+    sudo apt-get update
+    sudo apt-get install -q -y ansible python-mysqldb
+fi
 
 ansible-playbook util/ansible/update.yml --inventory=util/ansible/hosts --extra-vars "app_env=production"

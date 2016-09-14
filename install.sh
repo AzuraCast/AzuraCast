50f897d28c4d5a534b8df8af9b904b85ada9c823
Buster Silver <loobalightdark@gmail.com>
2016-09-13 21:16:39 -0500

    Add the same "--dev" flag to install.sh (in case)

--- install.sh
+++ install.sh
@@ -1,9 +1,28 @@
 #!/usr/bin/env bash
 
-sudo apt-get update
-sudo apt-get install -q -y software-properties-common
-sudo apt-add-repository ppa:ansible/ansible
-sudo apt-get update
-sudo apt-get install -q -y ansible python-mysqldb
+while test $# -gt 0; do
+    case "$1" in
+        --dev)
+            APP_ENV="development"
+            shift
+            ;;
 
-ansible-playbook util/ansible/deploy.yml --inventory=util/ansible/hosts --extra-vars "app_env=production"
+        *)
+            break
+            ;;
+    esac
+done
+
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' ansible|grep "install ok installed")
+echo Checking for Ansible: $PKG_OK
+
+if [ "" == "$PKG_OK" ]; then
+    sudo apt-get update
+    sudo apt-get install -q -y software-properties-common
+    sudo apt-add-repository ppa:ansible/ansible
+    sudo apt-get update
+    sudo apt-get install -q -y ansible python-mysqldb
+fi
+
+echo "Installing AzuraCast (Environment: $APP_ENV)"
+ansible-playbook util/ansible/deploy.yml --inventory=util/ansible/hosts --extra-vars "app_env=$APP_ENV"

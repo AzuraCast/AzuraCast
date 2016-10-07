cfb56c10db7473fe95e3fbaff73b5fc4de468070
Buster Silver <loobalightdark@gmail.com>
2016-10-07 03:13:49 -0500

    Remove jPlayer and replace with a small direct connector to HTML5 audio.

--- web/static/js/radio.js
+++ web/static/js/radio.js
@@ -2,14 +2,16 @@
  * Radio Player Script
  */
 
-var volume = 70,
-    is_playing;
+var volume = 55,
+    is_playing = false,
+    player,
+    $player;
 
 $(function() {
 
     // Check webstorage for existing volume preference.
     if (store.enabled && store.get('player_volume') !== undefined)
-        volume = store.get('player_volume', 70);
+        volume = store.get('player_volume', volume);
 
     $('.btn-audio').on('click', function(e) {
         e.preventDefault();
@@ -17,19 +19,91 @@ $(function() {
         return false;
     });
 
+    // Create audio element.
+    player = document.createElement('audio');
+    $player = $(player);
+
+    setVolume(volume/100);
+
+    var can_play_mp3 = !!(player.canPlayType && player.canPlayType('audio/mpeg;').replace(/no/, ''));
+    if (!can_play_mp3)
+        console.error('This browser cannot play MP3 files directly.');
+
+    // Handle events.
+    $player.on('play', function(e) {
+
+        is_playing = true;
+
+        $('.jp-unmute').hide();
+        $('#radio-player-controls').addClass('jp-state-playing');
+
+        var volume_percent = Math.round($player.volume * 100);
+        $('.jp-volume-bar-value').css('width', volume_percent+'%');
+
+    });
+
+    $player.on('ended', function(e) {
+
+        $('#radio-player-controls').removeClass('jp-state-playing');
+
+    });
+
+    // Handle button clicks.
+
+    $('.jp-pause').on('click', function(e) {
+        stopAllPlayers();
+    });
+
+    $('.jp-mute').on('click', function(e) {
+        player.volume = 0;
+        $('.jp-unmute').show();
+        $('.jp-mute').hide();
+    });
+
+    $('.jp-unmute').on('click', function(e) {
+        setVolume(volume);
+        $('.jp-unmute').hide();
+        $('.jp-mute').show();
+    });
+
+    $('.jp-volume-bar').on('click', function(e) {
+
+        var $bar = $(e.currentTarget),
+            offset = $bar.offset(),
+            x = e.pageX - offset.left,
+            w = $bar.width(),
+            y = $bar.height() - e.pageY + offset.top,
+            h = $bar.height();
+
+        setVolume(x/w);
+    });
+
 });
+
+function setVolume(new_volume)
+{
+    console.log('New volume: '+new_volume);
+
+    volume = new_volume;
+
+    var volume_percent = Math.round(volume * 100);
+    $('.jp-volume-bar-value').css('width', volume_percent+'%');
+
+    var v = Math.pow(volume,3);
+    player.volume = v;
+
+    if (store.enabled)
+        store.set('player_volume', volume*100);
+}
 
 function handlePlayClick(audio_source)
 {
     btn = $('.btn-audio[data-url="'+audio_source+'"]');
 
-    if (btn.hasClass('playing'))
-    {
+    if (btn.hasClass('playing')) {
         stopAllPlayers();
-    }
-    else
-    {
-        if ($('.btn-audio.playing').length != 0)
+    } else {
+        if (is_playing)
             stopAllPlayers();
 
         playAudio(audio_source);
@@ -40,105 +114,17 @@ function handlePlayClick(audio_source)
 
 function playAudio(source_url)
 {
-    $("#radio-player").jPlayer({
-        ready: function (event) {
-            ready = true;
-
-            startPlayer(source_url);
-        },
-        pause: function() {
-            stopAllPlayers();
-        },
-        play: function() {
-            is_playing = true;
-        },
-        suspend: function(event) {
-            console.log('Stream Suspended');
-
-            is_playing = false;
-        },
-        error: function(event) {
-            var error_details = event.jPlayer.error;
-            console.log('Error: '+error_details.message+' - '+error_details.hint);
-            is_playing = false;
-
-            // Auto-replay if Media URL load failure.
-            if (error_details.message == 'Media URL could not be loaded.')
-                startPlayer(source_url);
-            else
-                stopAllPlayers();
-        },
-        volumechange: function(event) {
-            volume = Math.round(event.jPlayer.options.volume * 100);
-
-            // Persist to local webstorage if enabled.
-            if (store.enabled)
-                store.set('player_volume', volume);
-        },
-        wmode: 'window',
-        swfPath: APP_ContentPath+'/vendors/bower_components/jPlayer/dist/jplayer/jquery.jplayer.swf',
-        solution: (canPlayMp3()) ? 'html, flash' : 'flash',
-        supplied: 'mp3',
-        preload: 'none',
-        volume: (volume/100),
-        muted: false,
-        backgroundColor: '#000000',
-        cssSelectorAncestor: '#radio-player-controls',
-        errorAlerts: false,
-        warningAlerts: false
-    });
-}
-
-function startPlayer(source_url)
-{
-    var stream = {
-        title: "AzuraCast",
-        mp3: source_url
-    };
-
-    $("#radio-player").jPlayer("setMedia", stream).jPlayer("play");
-
-    is_playing = true;
-    // check_interval = setInterval('checkPlayer()', 1500);
+    player.src = source_url;
+    player.play();
 }
 
 function stopAllPlayers()
 {
-    try
-    {
-        $('#radio-player').jPlayer('stop');
-    }
-    catch(e) {}
-
-    try
-    {
-        $('#radio-player').jPlayer("clearMedia").jPlayer("destroy");
-    }
-    catch(e) {}
+    player.pause();
+    player.src = '';
 
     is_playing = false;
     $('.btn-audio').removeClass('playing').find('i').removeClass('zmdi-stop').addClass('zmdi-play');
-}
 
-function canPlayMp3()
-{
-    if (isIE())
-        return false;
-
-    if (isSteam())
-        return false;
-
-    var a = document.createElement('audio');
-    var can_play_mp3 = !!(a.canPlayType && a.canPlayType('audio/mpeg;').replace(/no/, ''));
-    return can_play_mp3;
-}
-
-function isIE () {
-    var myNav = navigator.userAgent.toLowerCase();
-    return (myNav.indexOf('msie') != -1) ? parseInt(myNav.split('msie')[1]) : false;
-}
-
-function isSteam() {
-    var myNav = navigator.userAgent.toLowerCase();
-    return (myNav.indexOf('gameoverlay') != -1) ? true : false;
+    $('#radio-player-controls').removeClass('jp-state-playing');
 }

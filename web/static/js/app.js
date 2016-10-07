9a927d97f1ea6223b8cdb342c4cdab910f94b19c
Buster Silver <loobalightdark@gmail.com>
2016-10-07 05:06:15 -0500

    Fix notifications, remove icons on admin sidebar, and remove jPlayer from codebase.

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1354,30 +1354,26 @@ $(document).ready(function(){
     }
 });
 
-$(window).load(function() {
-
-    function notify(message, type) {
-        $.growl({
-            message: message
-        }, {
-            type: type,
-            allow_dismiss: true,
-            label: 'Cancel',
-            className: 'btn-xs btn-inverse align-right',
-            placement: {
-                from: 'top',
-                align: 'right'
-            },
-            delay: 10000,
-            animate: {
-                enter: 'animated fadeIn',
-                exit: 'animated fadeOut'
-            },
-            offset: {
-                x: 20,
-                y: 85
-            }
-        });
-    }
-
-});
+function notify(message, type) {
+    $.growl({
+        message: message
+    }, {
+        type: type,
+        allow_dismiss: true,
+        label: 'Cancel',
+        className: 'btn-xs btn-inverse align-right',
+        placement: {
+            from: 'top',
+            align: 'right'
+        },
+        delay: 10000,
+        animate: {
+            enter: 'animated fadeIn',
+            exit: 'animated fadeOut'
+        },
+        offset: {
+            x: 20,
+            y: 85
+        }
+    });
+}
--- web/static/js/inc/notify.js
+++ web/static/js/inc/notify.js
@@ -1,27 +1,23 @@
-$(window).load(function() {
-
-    function notify(message, type) {
-        $.growl({
-            message: message
-        }, {
-            type: type,
-            allow_dismiss: true,
-            label: 'Cancel',
-            className: 'btn-xs btn-inverse align-right',
-            placement: {
-                from: 'top',
-                align: 'right'
-            },
-            delay: 10000,
-            animate: {
-                enter: 'animated fadeIn',
-                exit: 'animated fadeOut'
-            },
-            offset: {
-                x: 20,
-                y: 85
-            }
-        });
-    }
-
-});
+function notify(message, type) {
+    $.growl({
+        message: message
+    }, {
+        type: type,
+        allow_dismiss: true,
+        label: 'Cancel',
+        className: 'btn-xs btn-inverse align-right',
+        placement: {
+            from: 'top',
+            align: 'right'
+        },
+        delay: 10000,
+        animate: {
+            enter: 'animated fadeIn',
+            exit: 'animated fadeOut'
+        },
+        offset: {
+            x: 20,
+            y: 85
+        }
+    });
+}

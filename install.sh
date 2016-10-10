143d16a41886ff58f984b04a530c76d1bb82dc2b
Buster Silver <loobalightdark@gmail.com>
2016-10-10 02:07:33 -0500

    Switch back to ansible PPA for initial install.

--- install.sh
+++ install.sh
@@ -19,11 +19,13 @@ echo Checking for Ansible: $PKG_OK
 if [ "" == "$PKG_OK" ]; then
     sudo apt-get update
     sudo apt-get install -q -y software-properties-common
+
+    sudo add-apt-repository -y ppa:ansible/ansible
     sudo add-apt-repository -y ppa:fkrull/deadsnakes
     sudo apt-get update
 
-    sudo apt-get install -q -y python2.7 python-pip python-mysqldb
-    sudo pip install --upgrade pip ansible
+    sudo apt-get install -q -y python2.7 python-pip python-mysqldb ansible
+    sudo pip install --upgrade pip
 fi
 
 APP_ENV="${APP_ENV:-production}"
--- util/ansible_setup.sh
+++ util/ansible_setup.sh
@@ -18,8 +18,12 @@ sudo apt-get install -q -y software-properties-common
 sudo add-apt-repository -y ppa:fkrull/deadsnakes
 sudo apt-get update
 
-sudo apt-get install -q -y python2.7 python-pip python-mysqldb
-sudo pip install --upgrade pip ansible
+sudo add-apt-repository -y ppa:ansible/ansible
+sudo add-apt-repository -y ppa:fkrull/deadsnakes
+sudo apt-get update
+
+sudo apt-get install -q -y python2.7 python-pip python-mysqldb ansible
+sudo pip install --upgrade pip
 
 cat > /home/ubuntu/.ansible.cfg <<EOF
 [defaults]

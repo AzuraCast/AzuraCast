45453b90bcdb035b778365e4b31298dc1823783a
Buster Silver <loobalightdark@gmail.com>
2016-10-22 10:30:16 -0500

    Clean up original SSH scripts associated with Ansible tasks, as the Ansible scripts have evolved past them now.

--- util/ansible/roles/azuracast-cron/original.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Add cron job
-echo "Installing cron job..."
-crontab -u azuracast $www_base/util/vagrant_cron
-
-service cron start
--- util/ansible/roles/azuracast-db/original.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Set up DB.
-echo "Setting up database..."
-
-cd $util_base
-
-phpuser doctrine.php orm:schema-tool:create
-phpuser cli.php cache:clear

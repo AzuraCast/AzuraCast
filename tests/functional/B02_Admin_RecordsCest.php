bd7ef75ba8830f64ffde93e168d14c0ece1d0d9a
Buster Silver <loobalightdark@gmail.com>
2016-10-23 11:10:00 -0500

    Improve coverage on admin settings.

--- tests/functional/B02_Admin_RecordsCest.php
+++ tests/functional/B02_Admin_RecordsCest.php
@@ -9,13 +9,54 @@ class B02_Admin_RecordsCest extends CestAbstract
     {
         $I->wantTo('See administration records.');
 
+        // Users
+
         $I->amOnPage('/admin/users');
         $I->see($this->login_username);
+
+        $I->click('Edit');
+
+        $I->submitForm('.form', []);
+
+        $I->seeCurrentUrlEquals('/admin/users');
+        $I->see($this->login_username);
+
+        // Permissions
 
         $I->amOnPage('/admin/permissions');
         $I->see('Super Administrator');
 
+        $I->click('Edit');
+
+        $I->submitForm('.form', [
+            'app_form' => [
+                'name' => 'Test Administrator',
+            ],
+        ]);
+
+        $I->seeCurrentUrlEquals('/admin/permissions');
+        $I->see('Test Administrator');
+
+        // Stations
+
         $I->amOnPage('/admin/stations');
         $I->see('Functional Test Radio');
+
+        $I->click('Edit');
+
+        $I->submitForm('.form', [
+            'app_form' => [
+                'name' => 'Modification Test Radio',
+            ],
+        ]);
+
+        $I->seeCurrentUrlEquals('/admin/stations');
+        $I->see('Modification Test Radio');
+
+        // Settings
+
+        $I->amOnPage('/admin/settings');
+        $I->submitForm('.form', []);
+        $I->seeCurrentUrlEquals('/admin/settings');
     }
 }

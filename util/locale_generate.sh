e0e864e585cf65cd46657ff6b9875ba688e5fcb6
Buster Silver <loobalightdark@gmail.com>
2017-04-12 23:55:11 -0500

    Locale update.

--- util/locale_generate.sh
+++ util/locale_generate.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-find /var/azuracast/www -type f \( -name '*.php' -or -name '*.phtml' \) -print > /var/azuracast/list
+find /var/azuracast/www/app -type f \( -name '*.php' -or -name '*.phtml' \) -print > /var/azuracast/list
 xgettext --files-from=/var/azuracast/list --language=PHP -o /var/azuracast/www/app/locale/default.pot

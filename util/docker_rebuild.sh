92d8e92b11ce1fac3c0b0646fc0e3ebe0fa1569d
Buster Silver <loobalightdark@gmail.com>
2017-06-01 05:25:15 -0500

    Remove redundant network configuration and update nginx to properly handle proxying of station audio.

--- util/docker_rebuild.sh
+++ util/docker_rebuild.sh
@@ -5,5 +5,6 @@ docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
 docker volume prune -f
 
-docker-compose up -d --build
+docker-compose build
 docker-compose run web azuracast_install --dev
+docker-compose up -d

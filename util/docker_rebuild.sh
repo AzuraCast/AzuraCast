bd69b08b6a2b3fcc50a9e1d359caf63aefd987e3
Buster Silver <loobalightdark@gmail.com>
2017-06-02 04:57:42 -0500

    Add building steps to a specific dockerfile that isn't the main one.

--- util/docker_rebuild.sh
+++ util/docker_rebuild.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-# shopt -s expand_aliases
-# alias docker-compose-local='docker-compose -f docker-compose.yml '
+shopt -s expand_aliases
+alias docker-compose-local='docker-compose -f docker-compose.yml -f docker-compose.build.yml '
 
-docker-compose kill
+docker-compose-local down -v
 docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
 docker volume prune -f
 
-docker-compose build
-docker-compose run web azuracast_install --dev
-docker-compose up -d
+docker-compose-local build
+docker-compose-local run web azuracast_install --dev
+docker-compose-local up -d

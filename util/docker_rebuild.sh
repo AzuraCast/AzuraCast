57c30177fdc7b5bc8d103be22f324071fc59e911
Buster Silver <loobalightdark@gmail.com>
2017-06-02 10:40:19 -0500

    Split dev stuff in docker into its own compose file to avoid Travis tripping over it.

--- util/docker_rebuild.sh
+++ util/docker_rebuild.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
 
-shopt -s expand_aliases
-alias docker-compose-local='docker-compose -f docker-compose.yml -f docker-compose.build.yml '
-
-docker-compose-local down -v
+./docker-compose-dev.sh down -v
 docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
 docker volume prune -f
 
-docker-compose-local build
-docker-compose-local run web azuracast_install --dev
-docker-compose-local up -d
+./docker-compose-dev.sh build
+./docker-compose-dev.sh run web azuracast_install --dev
+./docker-compose-dev.sh up -d

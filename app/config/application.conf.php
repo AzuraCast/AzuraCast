227c97126b334a18763fa67c2d4149cd00fe35ec
Buster Silver <loobalightdark@gmail.com>
2017-06-05 06:05:17 -0500

    Add Hungarian language to configuration dropdown. [skip ci]

--- app/config/application.conf.php
+++ app/config/application.conf.php
@@ -44,13 +44,14 @@ $config = [
         'default' => 'en_US.UTF-8',
         'supported' => [
             'en_US.UTF-8' => 'English (Default)',
-            'de_DE.UTF-8' => 'Deutsch',
-            'es_ES.UTF-8' => 'Español',
-            'fr_FR.UTF-8' => 'Français',
-            'pl_PL.UTF-8' => 'Polski',
-            // 'pt_PT.UTF-8' => 'Português',
-            'ru_RU.UTF-8' => 'Русский язык',
-            // 'sv_SE.UTF-8' => 'Svenska',
+            'de_DE.UTF-8' => 'Deutsch',             // German
+            'es_ES.UTF-8' => 'Español',             // Spanish
+            'fr_FR.UTF-8' => 'Français',            // French
+            'hu_HU.UTF-8' => 'magyar',              // Hungarian
+            'pl_PL.UTF-8' => 'Polski',              // Polish
+            'ru_RU.UTF-8' => 'Русский язык',        // Russian
+            // 'pt_PT.UTF-8' => 'Português',        // Portuguese
+            // 'sv_SE.UTF-8' => 'Svenska',          // Swedish
         ],
     ],
 

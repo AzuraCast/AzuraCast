a885444411f6c0260e21f3263a5c004ea38849de
Buster Silver <loobalightdark@gmail.com>
2017-06-07 10:25:49 -0500

    Fix update revision setting and remove supervisor from service reboots due to issues on 14.04. [skip ci]

--- update.sh
+++ update.sh
@@ -30,7 +30,7 @@ if [ "" == "$PKG_OK" ]; then
 fi
 
 APP_ENV="${APP_ENV:-production}"
-UPDATE_REVISION="{$UPDATE_REVISION:-12"
+UPDATE_REVISION="{$UPDATE_REVISION:-13}"
 
 echo "Updating AzuraCast (Environment: $APP_ENV)"
 

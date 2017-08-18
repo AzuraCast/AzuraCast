b1d35e85f402baabc5171bcbeee4c4f69bff27db
Buster "Silver Eagle" Neece <loobalightdark@gmail.com>
2017-08-18 05:35:56 -0500

    #212 -- Make MariaDB updates independent of DB provisioning.

--- update.sh
+++ update.sh
@@ -30,7 +30,7 @@ if [ "" == "$PKG_OK" ]; then
 fi
 
 APP_ENV="${APP_ENV:-production}"
-UPDATE_REVISION="{$UPDATE_REVISION:-14}"
+UPDATE_REVISION="{$UPDATE_REVISION:-15}"
 
 echo "Updating AzuraCast (Environment: $APP_ENV)"
 

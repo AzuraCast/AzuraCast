4294ff638f5709f91a2bad277261ea7debb076c7
Buster Silver <loobalightdark@gmail.com>
2017-09-08 03:28:57 -0500

    #225 -- Trigger ShoutCast DNAS upgrade for existing trad. installs.

--- update.sh
+++ update.sh
@@ -30,7 +30,7 @@ if [ "" == "$PKG_OK" ]; then
 fi
 
 APP_ENV="${APP_ENV:-production}"
-UPDATE_REVISION="{$UPDATE_REVISION:-16}"
+UPDATE_REVISION="{$UPDATE_REVISION:-17}"
 
 echo "Updating AzuraCast (Environment: $APP_ENV)"
 

8852d23abc0725933ff132624549da71596ebb13
Buster "Silver Eagle" Neece <loobalightdark@gmail.com>
2017-09-25 12:55:02 -0500

    More static asset cleanup!

--- web/static/bower_components/codemirror/src/display/selection.js
+++ web/static/bower_components/codemirror/src/display/selection.js
@@ -1,7 +1,7 @@
 import { Pos } from "../line/pos"
 import { visualLine } from "../line/spans"
 import { getLine } from "../line/utils_line"
-import { charCoords, cursorCoords, displayWidth, paddingH } from "../measurement/position_measurement"
+import { charCoords, cursorCoords, displayWidth, paddingH, wrappedLineExtentChar } from "../measurement/position_measurement"
 import { getOrder, iterateBidiSections } from "../util/bidi"
 import { elt } from "../util/dom"
 
@@ -9,13 +9,13 @@ export function updateSelection(cm) {
   cm.display.input.showSelection(cm.display.input.prepareSelection())
 }
 
-export function prepareSelection(cm, primary) {
+export function prepareSelection(cm, primary = true) {
   let doc = cm.doc, result = {}
   let curFragment = result.cursors = document.createDocumentFragment()
   let selFragment = result.selection = document.createDocumentFragment()
 
   for (let i = 0; i < doc.sel.ranges.length; i++) {
-    if (primary === false && i == doc.sel.primIndex) continue
+    if (!primary && i == doc.sel.primIndex) continue
     let range = doc.sel.ranges[i]
     if (range.from().line >= cm.display.viewTo || range.to().line < cm.display.viewFrom) continue
     let collapsed = range.empty()
@@ -46,6 +46,8 @@ export function drawSelectionCursor(cm, head, output) {
   }
 }
 
+function cmpCoords(a, b) { return a.top - b.top || a.left - b.left }
+
 // Draws the given range as a highlighted selection
 function drawSelectionRange(cm, range, output) {
   let display = cm.display, doc = cm.doc
@@ -70,30 +72,48 @@ function drawSelectionRange(cm, range, output) {
       return charCoords(cm, Pos(line, ch), "div", lineObj, bias)
     }
 
-    iterateBidiSections(getOrder(lineObj, doc.direction), fromArg || 0, toArg == null ? lineLen : toArg, (from, to, dir) => {
-      let leftPos = coords(from, "left"), rightPos, left, right
-      if (from == to) {
-        rightPos = leftPos
-        left = right = leftPos.left
-      } else {
-        rightPos = coords(to - 1, "right")
-        if (dir == "rtl") { let tmp = leftPos; leftPos = rightPos; rightPos = tmp }
-        left = leftPos.left
-        right = rightPos.right
+    let order = getOrder(lineObj, doc.direction)
+    iterateBidiSections(order, fromArg || 0, toArg == null ? lineLen : toArg, (from, to, dir, i) => {
+      let fromPos = coords(from, dir == "ltr" ? "left" : "right")
+      let toPos = coords(to - 1, dir == "ltr" ? "right" : "left")
+      if (dir == "ltr") {
+        let fromLeft = fromArg == null && from == 0 ? leftSide : fromPos.left
+        let toRight = toArg == null && to == lineLen ? rightSide : toPos.right
+        if (toPos.top - fromPos.top <= 3) { // Single line
+          add(fromLeft, toPos.top, toRight - fromLeft, toPos.bottom)
+        } else { // Multiple lines
+          add(fromLeft, fromPos.top, null, fromPos.bottom)
+          if (fromPos.bottom < toPos.top) add(leftSide, fromPos.bottom, null, toPos.top)
+          add(leftSide, toPos.top, toPos.right, toPos.bottom)
+        }
+      } else if (from < to) { // RTL
+        let fromRight = fromArg == null && from == 0 ? rightSide : fromPos.right
+        let toLeft = toArg == null && to == lineLen ? leftSide : toPos.left
+        if (toPos.top - fromPos.top <= 3) { // Single line
+          add(toLeft, toPos.top, fromRight - toLeft, toPos.bottom)
+        } else { // Multiple lines
+          let topLeft = leftSide
+          if (i) {
+            let topEnd = wrappedLineExtentChar(cm, lineObj, null, from).end
+            // The coordinates returned for an RTL wrapped space tend to
+            // be complete bogus, so try to skip that here.
+            topLeft = coords(topEnd - (/\s/.test(lineObj.text.charAt(topEnd - 1)) ? 2 : 1), "left").left
+          }
+          add(topLeft, fromPos.top, fromRight - topLeft, fromPos.bottom)
+          if (fromPos.bottom < toPos.top) add(leftSide, fromPos.bottom, null, toPos.top)
+          let botWidth = null
+          if (i < order.length  - 1 || true) {
+            let botStart = wrappedLineExtentChar(cm, lineObj, null, to).begin
+            botWidth = coords(botStart, "right").right - toLeft
+          }
+          add(toLeft, toPos.top, botWidth, toPos.bottom)
+        }
       }
-      if (fromArg == null && from == 0) left = leftSide
-      if (rightPos.top - leftPos.top > 3) { // Different lines, draw top part
-        add(left, leftPos.top, null, leftPos.bottom)
-        left = leftSide
-        if (leftPos.bottom < rightPos.top) add(left, leftPos.bottom, null, rightPos.top)
-      }
-      if (toArg == null && to == lineLen) right = rightSide
-      if (!start || leftPos.top < start.top || leftPos.top == start.top && leftPos.left < start.left)
-        start = leftPos
-      if (!end || rightPos.bottom > end.bottom || rightPos.bottom == end.bottom && rightPos.right > end.right)
-        end = rightPos
-      if (left < leftSide + 1) left = leftSide
-      add(left, rightPos.top, right - left, rightPos.bottom)
+
+      if (!start || cmpCoords(fromPos, start) < 0) start = fromPos
+      if (cmpCoords(toPos, start) < 0) start = toPos
+      if (!end || cmpCoords(fromPos, end) < 0) end = fromPos
+      if (cmpCoords(toPos, end) < 0) end = toPos
     })
     return {start: start, end: end}
   }

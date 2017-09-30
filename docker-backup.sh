47471928d4cb1f2a73e3f411ee6fd5ee37641eab
Buster Silver <loobalightdark@gmail.com>
2017-09-30 08:00:46 -0500

    Closes #235 -- Allow fully arbitrary backup file path.

--- docker-backup.sh
+++ docker-backup.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
 
-BACKUP_FILENAME=${1:-"backup.tar.gz"}
+#
+# Usage:
+# ./docker-backup.sh [/custom/backup/dir/custombackupname.tar.gz]
+#
+
+BACKUP_PATH=${1:-"./backup.tar.gz"}
+
+BACKUP_DIR=`dirname "$BACKUP_PATH"`
+BACKUP_FILENAME=`basename "$BACKUP_PATH"`
 
 docker-compose down
 
-docker run --rm -v $(pwd):/backup \
+docker run --rm -v $BACKUP_DIR:/backup \
     -v azuracast_db_data:/azuracast/db \
     -v azuracast_influx_data:/azuracast/influx \
     -v azuracast_station_data:/azuracast/stations \
--- docker-restore.sh
+++ docker-restore.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
 
-BACKUP_FILENAME=${1:-"backup.tar.gz"}
+#
+# Usage:
+# ./docker-restore.sh [/custom/backup/dir/custombackupname.tar.gz]
+#
 
-if [ -f $BACKUP_FILENAME ]; then
+BACKUP_PATH=${1:-"./backup.tar.gz"}
+
+BACKUP_DIR=`dirname "$BACKUP_PATH"`
+BACKUP_FILENAME=`basename "$BACKUP_PATH"`
+
+if [ -f $BACKUP_PATH ]; then
    docker-compose down
 
     docker volume rm azuracast_db_data azuracast_influx_data azuracast_station_data
@@ -10,7 +18,7 @@ if [ -f $BACKUP_FILENAME ]; then
     docker volume create azuracast_influx_data
     docker volume create azuracast_station_data
 
-    docker run --rm -v $(pwd):/backup \
+    docker run --rm -v $BACKUP_DIR:/backup \
         -v azuracast_db_data:/azuracast/db \
         -v azuracast_influx_data:/azuracast/influx \
         -v azuracast_station_data:/azuracast/stations \
@@ -18,5 +26,6 @@ if [ -f $BACKUP_FILENAME ]; then
 
     docker-compose up -d
 else
-    echo "File $BACKUP_FILENAME does not exist in this directory. Nothing to restore."
+    echo "File $BACKUP_PATH does not exist in this directory. Nothing to restore."
+    exit 1
 fi

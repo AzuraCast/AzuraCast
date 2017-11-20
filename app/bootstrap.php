e973ffeaa17554d54fab757708ef62e3e71a1998
Buster "Silver Eagle" Neece <loobalightdark@gmail.com>
2017-11-20 02:23:39 -0600

    Minor change to harden session security in application.

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -51,6 +51,7 @@ ini_set('error_log',                    APP_INCLUDE_TEMP . '/php_errors.log');
 ini_set('error_reporting',              E_ALL & ~E_NOTICE & ~E_WARNING & ~E_STRICT);
 ini_set('session.use_only_cookies',     1);
 ini_set('session.cookie_lifetime',      86400);
+ini_set('session.use_strict_mode',      1);
 
 // Composer autoload.
 $autoloader = require(APP_INCLUDE_VENDOR . '/autoload.php');

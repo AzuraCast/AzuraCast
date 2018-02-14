000c12c41fc0f26a86177d519d2d96440711aa35
Buster "Silver Eagle" Neece <loobalightdark@gmail.com>
2018-02-14 08:38:59 -0600

    #422 -- A more robust removal and reconfiguration of nginx without nchan.

--- update.sh
+++ update.sh
@@ -30,7 +30,7 @@ if [ "" == "$PKG_OK" ]; then
 fi
 
 APP_ENV="${APP_ENV:-production}"
-UPDATE_REVISION="${UPDATE_REVISION:-22}"
+UPDATE_REVISION="${UPDATE_REVISION:-23}"
 
 echo "Updating AzuraCast (Environment: $APP_ENV, Update revision: $UPDATE_REVISION)"
 

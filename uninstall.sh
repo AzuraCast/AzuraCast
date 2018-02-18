df712d37ee073dd07f042b40f500b99c40e796e4
Buster "Silver Eagle" Neece <loobalightdark@gmail.com>
2018-02-18 14:46:42 -0600

    Add big honkin' warning and MariaDB removal info back.

--- uninstall.sh
+++ uninstall.sh
@@ -2,15 +2,25 @@
 
 APP_ENV="${APP_ENV:-production}"
 
-echo "Installing AzuraCast (Environment: $APP_ENV)"
-ansible-playbook util/ansible/uninstall.yml --inventory=util/ansible/hosts --extra-vars "app_env=$APP_ENV"
+read -p "WARNING: This operation is destructive and will uninstall software on this server. Continue? [y/N] " -n 1 -r
+echo
 
-echo " "
-echo "Uninstallation complete. Some components were not removed."
-echo " "
-echo "To automatically remove unnecessary packages, run:"
-echo "  apt-get autoremove"
-echo " "
-echo "To remove AzuraCast station data, run:"
-echo "  rm -rf /var/azuracast/stations"
-echo " "
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+
+    echo "Uninstalling AzuraCast..."
+    ansible-playbook util/ansible/uninstall.yml --inventory=util/ansible/hosts --extra-vars "app_env=$APP_ENV"
+
+    echo " "
+    echo "Uninstallation complete. Some components were not removed."
+    echo " "
+    echo "To automatically remove unnecessary packages, run:"
+    echo "  apt-get autoremove"
+    echo " "
+    echo "To remove MariaDB data, run:"
+    echo "  rm -rf /var/lib/mysql"
+    echo " "
+    echo "To remove AzuraCast station data, run:"
+    echo "  rm -rf /var/azuracast/stations"
+    echo " "
+
+fi

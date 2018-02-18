8ead594f3438b115a41b67dc03dd99786eabc027
Buster "Silver Eagle" Neece <loobalightdark@gmail.com>
2018-02-18 14:28:58 -0600

    Print uninstall message.

--- uninstall.sh
+++ uninstall.sh
@@ -4,3 +4,12 @@ APP_ENV="${APP_ENV:-production}"
 
 echo "Installing AzuraCast (Environment: $APP_ENV)"
 ansible-playbook util/ansible/uninstall.yml --inventory=util/ansible/hosts --extra-vars "app_env=$APP_ENV"
+
+echo "Uninstallation complete. Some components were not removed."
+echo " "
+echo "To remove MariaDB data, run:"
+echo "  rm -rf /var/lib/mysql"
+echo " "
+echo "To remove AzuraCast station data, run:"
+echo "  rm -rf /var/azuracast/stations"
+echo " "

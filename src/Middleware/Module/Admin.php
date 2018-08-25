7d4330665bc4a07427e59b0c51d083630280a6c2
Buster "Silver Eagle" Neece <loobalightdark@gmail.com>
2018-08-24 21:31:39 -0500

    Set active tabs on the station.

--- src/Middleware/Module/Admin.php
+++ src/Middleware/Module/Admin.php
@@ -31,8 +31,7 @@ class Admin
      */
     public function __invoke(Request $request, Response $response, $next): Response
     {
-        /** @var Entity\User $user */
-        $user = $request->getAttribute('user');
+        $user = $request->getUser();
 
         // Load dashboard.
         $panels = $this->dashboard_config;
@@ -58,8 +57,17 @@ class Admin
 
         $view = $request->getView();
 
+        $active_tab = null;
+        $current_route = $request->getAttribute('route');
+        if ($current_route instanceof Route) {
+            $route_parts = explode(':', $current_route->getName());
+            $active_tab = $route_parts[1];
+        }
+
         $view->admin_panels = $panels;
-        $view->sidebar = $view->render('admin/sidebar');
+        $view->sidebar = $view->render('admin/sidebar', [
+            'active_tab' => $active_tab,
+        ]);
 
         return $next($request, $response);
     }
--- src/Middleware/Module/Stations.php
+++ src/Middleware/Module/Stations.php
@@ -3,6 +3,7 @@ namespace App\Middleware\Module;
 
 use App\Http\Request;
 use App\Http\Response;
+use Slim\Route;
 
 /**
  * Module middleware for the /station pages.
@@ -25,7 +26,16 @@ class Stations
             'backend'   => $request->getStationBackend(),
         ]);
 
-        $view->sidebar = $view->render('stations/sidebar');
+        $active_tab = null;
+        $current_route = $request->getAttribute('route');
+        if ($current_route instanceof Route) {
+            $route_parts = explode(':', $current_route->getName());
+            $active_tab = $route_parts[1];
+        }
+
+        $view->sidebar = $view->render('stations/sidebar', [
+            'active_tab' => $active_tab,
+        ]);
 
         return $next($request, $response);
     }

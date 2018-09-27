b3289fa6244852d3db87cc94f86d2f62fba2a8db
Buster "Silver Eagle" Neece <loobalightdark@gmail.com>
2018-09-27 12:45:10 -0500

    Use Icecast-KH for all demo purposes.

--- src/Entity/Fixture/Station.php
+++ src/Entity/Fixture/Station.php
@@ -13,7 +13,7 @@ class Station extends AbstractFixture
         $station = new Entity\Station;
         $station->setName('AzuraTest Radio');
         $station->setDescription('A test radio station.');
-        $station->setFrontendType(Adapters::FRONTEND_SHOUTCAST);
+        $station->setFrontendType(Adapters::FRONTEND_ICECAST);
         $station->setBackendType(Adapters::BACKEND_LIQUIDSOAP);
         $station->setRadioBaseDir('/var/azuracast/stations/azuratest_radio');
 

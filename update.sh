01375ec044cb7af4a7257eb6e025ade5afdec3f4
CodeSteele <31152835+CodeSteele@users.noreply.github.com>
2018-10-21 00:42:22 -0400

    Upgrade to our own fork of Icecast-KH (#877)
    * Upgrade to our own fork of Icecast-KH
    * Update role for radio

--- update.sh
+++ update.sh
@@ -33,7 +33,7 @@ else
 fi
 
 APP_ENV="${APP_ENV:-production}"
-UPDATE_REVISION="${UPDATE_REVISION:-25}"
+UPDATE_REVISION="${UPDATE_REVISION:-26}"
 
 echo "Updating AzuraCast (Environment: $APP_ENV, Update revision: $UPDATE_REVISION)"
 

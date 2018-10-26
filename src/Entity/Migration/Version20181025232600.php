fcfe89af27ef3258d15b71f0d1657b88b84c624c
Vaalyn <vaalyndev@gmail.com>
2018-10-26 12:39:39 +0200

    Add reactivate_at to station_streamers table

---
+++ src/Entity/Migration/Version20181025232600.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+
+namespace App\Entity\Migration;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Add reactivate_at to station_streamers table
+ */
+final class Version20181025232600 extends AbstractMigration
+{
+	public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE station_streamers ADD reactivate_at INT DEFAULT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE station_streamers DROP reactivate_at');
+    }
+}

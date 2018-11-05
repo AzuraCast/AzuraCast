768d1e72a141123a2270520474cee9894b45b3fb
Buster "Silver Eagle" Neece <loobalightdark@gmail.com>
2018-11-04 22:38:04 -0600

    Specify path on influxdb dump

--- docker-migrate.sh
+++ docker-migrate.sh
@@ -30,8 +30,8 @@ read -n 1 -s -r -p "MySQL exported. Press any key to continue (Export InfluxDB).
 # Dump InfluxDB data
 mkdir -p /var/azuracast/migration
 
-influxd backup migration/influxdb
-influxd backup -database stations migration/influxdb
+influxd backup ./migration/influxdb
+influxd backup -database stations ./migration/influxdb
 
 read -n 1 -s -r -p "InfluxDB exported. Press any key to continue (Install Docker)..."
 

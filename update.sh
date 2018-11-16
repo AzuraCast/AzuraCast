9792875f647ecceab96e29eee63f4cfea48979da
CodeSteele <31152835+CodeSteele@users.noreply.github.com>
2018-11-16 04:23:05 -0500

    Bug/liquidsoap segfaulting (#951)
    * Update to duppy 0.7.4
    
    * Bump duppy to 0.8.0

--- update.sh
+++ update.sh
@@ -33,7 +33,7 @@ else
 fi
 
 APP_ENV="${APP_ENV:-production}"
-UPDATE_REVISION="${UPDATE_REVISION:-28}"
+UPDATE_REVISION="${UPDATE_REVISION:-29}"
 
 echo "Updating AzuraCast (Environment: $APP_ENV, Update revision: $UPDATE_REVISION)"
 

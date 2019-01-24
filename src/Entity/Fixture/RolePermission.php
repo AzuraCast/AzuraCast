29c8ba20773018354828e36b94f150b994854a46
Buster "Silver Eagle" Neece <loobalightdark@gmail.com>
2019-01-23 20:05:38 -0600

    Disable station administrator role for demo account.

--- src/Entity/Fixture/RolePermission.php
+++ src/Entity/Fixture/RolePermission.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Entity\Fixture;
 
+use App\Acl;
 use Doctrine\Common\DataFixtures\AbstractFixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
@@ -10,14 +11,16 @@ class RolePermission extends AbstractFixture implements DependentFixtureInterfac
 {
     public function load(ObjectManager $em)
     {
+        /** @var Entity\Station $station */
+        $station = $this->getReference('station');
+
         $permissions = [
             'admin_role' => [
-                'administer all',
+                [Acl::GLOBAL_ALL, null],
             ],
             'demo_role' => [
-                'administer api keys',
-                'administer stations',
-                'view administration',
+                [Acl::STATION_ALL, $station],
+                [Acl::STATION_VIEW, $station],
             ],
         ];
 
@@ -26,8 +29,7 @@ class RolePermission extends AbstractFixture implements DependentFixtureInterfac
             $role = $this->getReference($role_reference);
 
             foreach($perm_names as $perm_name) {
-                $rp = new Entity\RolePermission($role);
-                $rp->setActionName($perm_name);
+                $rp = new Entity\RolePermission($role, $perm_name[1], $perm_name[0]);
                 $em->persist($rp);
             }
         }

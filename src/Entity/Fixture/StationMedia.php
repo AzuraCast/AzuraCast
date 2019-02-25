948b5b7baba206f68a4c59650482323268bdc0e2
Buster "Silver Eagle" Neece <loobalightdark@gmail.com>
2019-02-25 14:29:12 -0600

    Fix how fixtures load music into the default playlist.

--- src/Entity/Fixture/StationMedia.php
+++ src/Entity/Fixture/StationMedia.php
@@ -5,6 +5,7 @@ use Doctrine\Common\DataFixtures\AbstractFixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 use App\Entity;
+use Symfony\Component\Finder\Finder;
 
 class StationMedia extends AbstractFixture implements DependentFixtureInterface
 {
@@ -12,48 +13,48 @@ class StationMedia extends AbstractFixture implements DependentFixtureInterface
     {
         $music_skeleton_dir = getenv('INIT_MUSIC_PATH');
 
-        if (!empty($music_skeleton_dir) && is_dir($music_skeleton_dir)) {
+        if (empty($music_skeleton_dir) || !is_dir($music_skeleton_dir)) {
+            return;
+        }
 
-            /** @var Entity\Station $station */
-            $station = $this->getReference('station');
+        /** @var Entity\Station $station */
+        $station = $this->getReference('station');
 
-            $station_media_dir = $station->getRadioMediaDir();
+        $station_media_dir = $station->getRadioMediaDir();
 
-            /** @var Entity\StationPlaylist $playlist */
-            $playlist = $this->getReference('station_playlist');
+        /** @var Entity\StationPlaylist $playlist */
+        $playlist = $this->getReference('station_playlist');
 
-            /** @var Entity\Repository\StationMediaRepository $media_repo */
-            $media_repo = $em->getRepository(Entity\StationMedia::class);
+        /** @var Entity\Repository\StationMediaRepository $media_repo */
+        $media_repo = $em->getRepository(Entity\StationMedia::class);
 
-            $directory = new \RecursiveDirectoryIterator($music_skeleton_dir);
-            $iterator = new \RecursiveIteratorIterator($directory);
+        $finder = (new Finder())
+            ->files()
+            ->in($music_skeleton_dir)
+            ->name('/^.+\.(mp3|aac|ogg|flac)$/i');
 
-            $i = 1;
+        foreach ($finder as $file) {
+            $file_path = $file->getPathname();
+            $file_base_name = basename($file_path);
 
-            foreach ($iterator as $file) {
-                if ($file->isDir()) {
-                    continue;
-                }
+            // Copy the file to the station media directory.
+            copy($file_path, $station_media_dir . '/' . $file_base_name);
 
-                $file_path = $file->getPathname();
-                $file_base_name = basename($file_path);
+            $media_row = $media_repo->getOrCreate($station, $file_base_name);
+            $em->persist($media_row);
 
-                // Copy the file to the station media directory.
-                copy($file_path, $station_media_dir . '/' . $file_base_name);
-
-                $media_row = $media_repo->getOrCreate($station, $file_base_name);
-                $em->persist($media_row);
-
-                // Add the file to the playlist.
-                $spm_row = new Entity\StationPlaylistMedia($playlist, $media_row);
-                $spm_row->setWeight($i);
-                $em->persist($spm_row);
-
-                $i++;
-            }
+            // Add the file to the playlist.
+            $spm_row = new Entity\StationPlaylistMedia($playlist, $media_row);
+            $spm_row->setWeight(1);
+            $em->persist($spm_row);
         }
 
         $em->flush();
+
+        /** @var Entity\Repository\StationPlaylistMediaRepository $spm_repo */
+        $spm_repo = $em->getRepository(Entity\StationPlaylistMedia::class);
+
+        $spm_repo->reshuffleMedia($playlist);
     }
 
     public function getDependencies()

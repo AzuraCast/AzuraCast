94eeef8587ed2335c1511a7da2817b03f1baf4c5
Buster Neece <loobalightdark@gmail.com>
2019-05-14 03:05:23 -0500

    Add name mapping for PlaylistTime field.

--- src/Form/Form.php
+++ src/Form/Form.php
@@ -11,6 +11,8 @@ class Form extends \AzuraForms\Form
     {
         array_unshift($this->field_namespaces, '\\App\\Form\\Field');
 
+        $this->field_name_conversions['playlisttime'] = 'PlaylistTime';
+
         parent::__construct($options, $defaults);
     }
 

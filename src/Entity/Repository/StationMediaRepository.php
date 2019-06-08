1c723792e3ccbb48be9d35ff9a38a5d8f1957085
Buster Neece <loobalightdark@gmail.com>
2019-06-08 02:27:32 -0500

    #1450 -- Further optimize image processing for JPEGs.

--- src/Entity/Repository/StationMediaRepository.php
+++ src/Entity/Repository/StationMediaRepository.php
@@ -278,43 +278,52 @@ class StationMediaRepository extends Repository
      */
     public function writeAlbumArt(Entity\StationMedia $media, $raw_art_string): bool
     {
-        $source_gd_image = imagecreatefromstring($raw_art_string);
+        $source_image_info = getimagesizefromstring($raw_art_string);
+        $source_image_width = $source_image_info[0] ?? 0;
+        $source_image_height = $source_image_info[1] ?? 0;
+        $source_mime_type = $source_image_info['mime'] ?? 'unknown';
 
-        if (!is_resource($source_gd_image)) {
-            return false;
-        }
-
-        // Crop the raw art to a 1200x1200 artboard.
         $dest_max_width = 1200;
         $dest_max_height = 1200;
 
-        $source_image_width = imagesx($source_gd_image);
-        $source_image_height = imagesy($source_gd_image);
+        $source_inside_dest = $source_image_width <= $dest_max_width && $source_image_height <= $dest_max_height;
 
-        if ($source_image_width <= $dest_max_width && $source_image_height <= $dest_max_height) {
-            $thumbnail_gd_image = $source_gd_image;
+        // Avoid GD entirely if it's already a JPEG within our parameters.
+        if ($source_mime_type === 'image/jpeg' && $source_inside_dest) {
+            $album_art = $raw_art_string;
         } else {
-            $source_aspect_ratio = $source_image_width / $source_image_height;
-            $thumbnail_aspect_ratio = $dest_max_width / $dest_max_height;
+            $source_gd_image = imagecreatefromstring($raw_art_string);
 
-            if ($thumbnail_aspect_ratio > $source_aspect_ratio) {
-                $thumbnail_image_width = (int)($dest_max_height * $source_aspect_ratio);
-                $thumbnail_image_height = $dest_max_height;
-            } else {
-                $thumbnail_image_width = $dest_max_width;
-                $thumbnail_image_height = (int)($dest_max_width / $source_aspect_ratio);
+            if (!is_resource($source_gd_image)) {
+                return false;
             }
 
-            $thumbnail_gd_image = imagecreatetruecolor($thumbnail_image_width, $thumbnail_image_height);
-            imagecopyresampled($thumbnail_gd_image, $source_gd_image, 0, 0, 0, 0, $thumbnail_image_width, $thumbnail_image_height, $source_image_width, $source_image_height);
+            // Crop the raw art to a 1200x1200 artboard.
+            if ($source_inside_dest) {
+                $thumbnail_gd_image = $source_gd_image;
+            } else {
+                $source_aspect_ratio = $source_image_width / $source_image_height;
+                $thumbnail_aspect_ratio = $dest_max_width / $dest_max_height;
+
+                if ($thumbnail_aspect_ratio > $source_aspect_ratio) {
+                    $thumbnail_image_width = (int)($dest_max_height * $source_aspect_ratio);
+                    $thumbnail_image_height = $dest_max_height;
+                } else {
+                    $thumbnail_image_width = $dest_max_width;
+                    $thumbnail_image_height = (int)($dest_max_width / $source_aspect_ratio);
+                }
+
+                $thumbnail_gd_image = imagecreatetruecolor($thumbnail_image_width, $thumbnail_image_height);
+                imagecopyresampled($thumbnail_gd_image, $source_gd_image, 0, 0, 0, 0, $thumbnail_image_width, $thumbnail_image_height, $source_image_width, $source_image_height);
+            }
+
+            ob_start();
+            imagejpeg($thumbnail_gd_image, NULL, 90);
+            $album_art = ob_get_clean();
+
+            imagedestroy($source_gd_image);
+            imagedestroy($thumbnail_gd_image);
         }
-
-        ob_start();
-        imagejpeg($thumbnail_gd_image, NULL, 90);
-        $album_art = ob_get_clean();
-
-        imagedestroy($source_gd_image);
-        imagedestroy($thumbnail_gd_image);
 
         $album_art_path = $media->getArtPath();
         $fs = $this->filesystem->getForStation($media->getStation());

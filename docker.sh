660ef0d44b193ce2d7919219c2ad36fb38746570
Buster Neece <loobalightdark@gmail.com>
2019-06-25 21:03:11 -0500

    Add sudo check to docker utility script.

--- docker.sh
+++ docker.sh
@@ -80,6 +80,13 @@ install() {
                 exit 1
             fi
 
+            if [[ ! $(which sudo) ]]; then
+                echo "Sudo does not appear to be installed."
+                echo "Install sudo using your host's package manager,"
+                echo "then continue installing using this script."
+                exit 1
+            fi
+
             COMPOSE_VERSION=`git ls-remote https://github.com/docker/compose | grep refs/tags | grep -oP "[0-9]+\.[0-9][0-9]+\.[0-9]+$" | tail -n 1`
             sudo sh -c "curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose"
             sudo chmod +x /usr/local/bin/docker-compose

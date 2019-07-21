d2bc72d2a8d5060a1e1b759c0e90dc8b34bf619e
Buster Neece <loobalightdark@gmail.com>
2019-07-20 19:05:40 -0500

    Fixes #1768 -- Properly handle the timecode "0" on PlaylistTime fields.

--- src/Form/Field/PlaylistTime.php
+++ src/Form/Field/PlaylistTime.php
@@ -16,7 +16,8 @@ class PlaylistTime extends Time
         // Handle the "time code" format used by the database entity,
         // which is just the regular 24-hour time minus the ":".
         $this->filters[] = function($new_value) {
-            if (!empty($new_value) && false === strpos($new_value, ':')) {
+            // Don't use regular empty() check because 0 (which is 00:00, 12:00AM) is considered empty.
+            if ('' !== $new_value && null !== $new_value && false === strpos($new_value, ':')) {
                 $time_code = str_pad($new_value, 4, '0', STR_PAD_LEFT);
                 return substr($time_code, 0, 2).':'.substr($time_code, 2);
             }

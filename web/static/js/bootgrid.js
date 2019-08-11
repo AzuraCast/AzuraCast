357f324d373bbbf9acc8aaa175ad97504d0418c7
Buster Neece <loobalightdark@gmail.com>
2019-08-11 14:24:01 -0500

    #1407 -- Make Bootgrid footer split at different breakpoints.

--- web/static/js/bootgrid.js
+++ web/static/js/bootgrid.js
@@ -12,6 +12,7 @@ $(function() {
 
     $.extend($.fn.bootgrid.Constructor.defaults.templates, {
         icon: "<i class=\"material-icons\">{{ctx.iconCss}}</i>",
-        paginationItem: "<li class=\"paginate_button page-item {{ctx.css}}\"><a data-page=\"{{ctx.page}}\" class=\"{{css.paginationButton}}\">{{ctx.text}}</a></li>"
+        paginationItem: "<li class=\"paginate_button page-item {{ctx.css}}\"><a data-page=\"{{ctx.page}}\" class=\"{{css.paginationButton}}\">{{ctx.text}}</a></li>",
+        footer: "<div id=\"{{ctx.id}}\" class=\"{{css.footer}}\"><div class=\"row\"><div class=\"col-lg-6\"><p class=\"{{css.pagination}}\"></p></div><div class=\"col-lg-6 infoBar\"><p class=\"{{css.infos}}\"></p></div></div></div>"
     });
 });

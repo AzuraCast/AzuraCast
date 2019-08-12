9eb2c849b0c26b2350aa1521a57aee2c0843a710
Buster Neece <loobalightdark@gmail.com>
2019-08-12 14:50:11 -0500

    #1407 -- Further clean up bootgrid footer layout.

--- web/static/js/bootgrid.js
+++ web/static/js/bootgrid.js
@@ -13,6 +13,6 @@ $(function() {
     $.extend($.fn.bootgrid.Constructor.defaults.templates, {
         icon: "<i class=\"material-icons\">{{ctx.iconCss}}</i>",
         paginationItem: "<li class=\"paginate_button page-item {{ctx.css}}\"><a data-page=\"{{ctx.page}}\" class=\"{{css.paginationButton}}\">{{ctx.text}}</a></li>",
-        footer: "<div id=\"{{ctx.id}}\" class=\"{{css.footer}}\"><div class=\"row\"><div class=\"col-lg-6\"><p class=\"{{css.pagination}}\"></p></div><div class=\"col-lg-6 infoBar\"><p class=\"{{css.infos}}\"></p></div></div></div>"
+        footer: "<div id=\"{{ctx.id}}\" class=\"{{css.footer}}\"><div class=\"row align-items-center\"><div class=\"col-lg-8\"><p class=\"{{css.pagination}}\"></p></div><div class=\"col-lg-4 py-3 infoBar\"><p class=\"{{css.infos}}\"></p></div></div></div>"
     });
 });

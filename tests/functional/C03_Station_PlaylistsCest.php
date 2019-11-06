09ae256ea2398cb31064b495915df2bb2b828d50
Buster "Silver Eagle" Neece <loobalightdark@gmail.com>
2019-11-06 15:34:51 -0600

    Remove previous Playlists pages from test suite.

--- tests/functional/C03_Station_PlaylistsCest.php
+++ tests/functional/C03_Station_PlaylistsCest.php
@@ -1,4 +1,5 @@
 <?php
+
 class C03_Station_PlaylistsCest extends CestAbstract
 {
     /**
@@ -10,23 +11,8 @@ class C03_Station_PlaylistsCest extends CestAbstract
         $I->wantTo('Create a station playlist.');
 
         $station_id = $this->test_station->getId();
-        $I->amOnPage('/station/'.$station_id.'/playlists');
+        $I->amOnPage('/station/' . $station_id . '/playlists');
 
         $I->see('Playlists');
-        $I->click('add', '#content');
-
-        $I->submitForm('.form', [
-            'name'          => 'Default Playlist',
-        ]);
-
-        $I->seeCurrentUrlEquals('/station/'.$station_id.'/playlists');
-
-        $I->see('Default Playlist');
-
-        $I->click(\Codeception\Util\Locator::lastElement('.btn-danger'));
-
-        $I->seeCurrentUrlEquals('/station/'.$station_id.'/playlists');
-
-        $I->dontSee('Default Playlist');
     }
 }

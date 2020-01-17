3b372a2d4605597f8e424e2f0a462f4145031454
Buster "Silver Eagle" Neece <loobalightdark@gmail.com>
2020-01-16 23:26:51 -0600

    #2003 -- Optimize memory usage on duplicate songs report.

--- src/Controller/Stations/Reports/DuplicatesController.php
+++ src/Controller/Stations/Reports/DuplicatesController.php
@@ -31,56 +31,38 @@ class DuplicatesController
     {
         $station = $request->getStation();
 
-        $media_raw = $this->em->createQuery(/** @lang DQL */ 'SELECT 
-            sm, s, spm, sp 
-            FROM App\Entity\StationMedia sm 
-            JOIN sm.song s 
-            LEFT JOIN sm.playlists spm 
-            LEFT JOIN spm.playlist sp 
-            WHERE sm.station_id = :station_id 
-            ORDER BY sm.mtime ASC')
-            ->setParameter('station_id', $station->getId())
+        $duplicateSongIdsRaw = $this->em->createQuery(/** @lang DQL */ 'SELECT
+            sm.song_id
+            FROM App\Entity\StationMedia sm
+            WHERE sm.station = :station GROUP BY sm.song_id HAVING COUNT(sm.id) > 1')
+            ->setParameter('station', $station)
             ->getArrayResult();
 
-        $dupes = [];
-        $songs_to_compare = [];
+        if (!empty($duplicateSongIdsRaw)) {
+            $duplicateSongIds = array_column($duplicateSongIdsRaw, 'song_id');
 
-        // Find exact duplicates and sort other songs into a searchable array.
-        foreach ($media_raw as $media_row) {
-            foreach ($media_row['playlists'] as $playlist_item) {
-                $media_row['playlists'][] = $playlist_item['playlist'];
-            }
+            $dupesRaw = $this->em->createQuery(/** @lang DQL */ 'SELECT
+                sm, s, spm, sp
+                FROM App\Entity\StationMedia sm
+                JOIN sm.song s
+                LEFT JOIN sm.playlists spm
+                LEFT JOIN spm.playlist sp
+                WHERE sm.station = :station
+                AND sm.song_id IN (:song_ids)
+                ORDER BY sm.song_id ASC, sm.mtime ASC')
+                ->setParameter('station', $station)
+                ->setParameter('song_ids', $duplicateSongIds)
+                ->getArrayResult();
 
-            if (isset($songs_to_compare[$media_row['song_id']])) {
-                $dupes[] = [$songs_to_compare[$media_row['song_id']], $media_row];
-            } else {
-                $songs_to_compare[$media_row['song_id']] = $media_row;
-            }
-        }
-
-        foreach ($songs_to_compare as $song_id => $media_row) {
-            unset($songs_to_compare[$song_id]);
-
-            $media_text = strtolower(preg_replace('/[^A-Za-z0-9]/', '', $media_row['song']['text']));
-
-            $song_dupes = [];
-            foreach ($songs_to_compare as $search_song_id => $search_media_row) {
-                $search_media_text = strtolower(preg_replace('/[^A-Za-z0-9]/', '', $search_media_row['song']['text']));
-                $similarity = levenshtein($media_text, $search_media_text);
-
-                if ($similarity <= 5) {
-                    $song_dupes[] = $search_media_row;
-                }
-            }
-
-            if (count($song_dupes) > 0) {
-                $song_dupes[] = $media_row;
-                $dupes[] = $song_dupes;
+            $dupes = [];
+            foreach ($dupesRaw as $row) {
+                $row['playlists'] = array_column($row['playlists'], 'playlist');
+                $dupes[$row['song_id']][] = $row;
             }
         }
 
         return $request->getView()->renderToResponse($response, 'stations/reports/duplicates', [
-            'dupes' => $dupes,
+            'dupes' => $dupes ?? [],
         ]);
     }
 

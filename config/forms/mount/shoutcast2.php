2c093abfe3fc097ccd998dd9f0f70e3c3d1b0f48
Buster "Silver Eagle" Neece <loobalightdark@gmail.com>
2020-01-21 00:26:21 -0600

    Update syntax on SHOUTcast authhash form.

--- config/forms/mount/shoutcast2.php
+++ config/forms/mount/shoutcast2.php
@@ -1,12 +1,13 @@
 <?php
+
 use App\Entity\StationMount;
 
-$form_config = require __DIR__.'/generic.php';
+$form_config = require __DIR__ . '/generic.php';
 
-$form_config['groups']['basic_info']['elements']['name'][1]['validator'] = function($text) {
+$form_config['groups']['basic_info']['elements']['name'][1]['validator'] = function ($text) {
     $forbidden_paths = ['listen', 'admin', 'statistics', '7.html'];
 
-    foreach($forbidden_paths as $forbidden_path) {
+    foreach ($forbidden_paths as $forbidden_path) {
         if (stripos($text, $forbidden_path) !== false) {
             return __('Stream path cannot include reserved keywords: %s', implode(', ', $forbidden_paths));
         }
@@ -19,11 +20,11 @@ $form_config['groups']['basic_info']['elements']['authhash'] = [
     'text',
     [
         'label' => __('YP Directory Authorization Hash'),
-        'description' => sprintf(__('If your stream is set to advertise to YP directories above, you must specify an authorization hash. You can manage authhashes <a href="%s" target="_blank">on the SHOUTcast web site</a>.'),
+        'description' => __('If your stream is set to advertise to YP directories above, you must specify an authorization hash. You can manage authhashes <a href="%s" target="_blank">on the SHOUTcast web site</a>.',
             'https://rmo.shoutcast.com'),
         'default' => '',
         'form_group_class' => 'col-sm-12',
-    ]
+    ],
 ];
 
 $form_config['groups']['autodj']['elements']['autodj_format'][1]['choices'] = [

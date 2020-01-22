6e98e0e207c109d3ee88f071ddc1f0cbaa2cc6a0
Buster "Silver Eagle" Neece <loobalightdark@gmail.com>
2020-01-22 04:47:15 -0600

    Remove dependency on util/ folder for CLI commands.

--- util/cli.php
+++ util/cli.php
@@ -1,17 +1,2 @@
 <?php
-error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT);
-ini_set('display_errors', 1);
-
-$autoloader = require dirname(__DIR__) . '/vendor/autoload.php';
-
-$app = App\AppFactory::create($autoloader, [
-    Azura\Settings::BASE_DIR => dirname(__DIR__),
-]);
-
-$di = $app->getContainer();
-
-App\Customization::initCli();
-
-/** @var Azura\Console\Application $cli */
-$cli = $di->get(Azura\Console\Application::class);
-$cli->run();
+include dirname(__DIR__) . '/bin/azuracast.php';

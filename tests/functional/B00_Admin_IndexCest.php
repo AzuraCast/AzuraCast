d2d5796cf6edd2aafe8c80bdce1826c65b76b4e7
Buster "Silver Eagle" Neece <buster@busterneece.com>
2020-03-01 10:40:15 -0600

    Update tests to reflect new URIs.

--- tests/functional/B00_Admin_IndexCest.php
+++ tests/functional/B00_Admin_IndexCest.php
@@ -1,4 +1,5 @@
 <?php
+
 class B00_Admin_IndexCest extends CestAbstract
 {
     /**
@@ -15,53 +16,5 @@ class B00_Admin_IndexCest extends CestAbstract
         $I->see('System Maintenance');
         $I->see('Users');
         $I->see('Stations');
-    }
-
-    /**
-     * @before setupComplete
-     * @before login
-     */
-    public function runNowPlayingSync(FunctionalTester $I)
-    {
-        $I->wantTo('Run now-playing synchronization task.');
-
-        $I->amOnPage('/admin/sync/nowplaying');
-        $I->seeInSource('Sync Task Output');
-    }
-
-    /**
-     * @before setupComplete
-     * @before login
-     */
-    public function runShortSync(FunctionalTester $I)
-    {
-        $I->wantTo('Run short synchronization task.');
-
-        $I->amOnPage('/admin/sync/short');
-        $I->seeInSource('Sync Task Output');
-    }
-
-    /**
-     * @before setupComplete
-     * @before login
-     */
-    public function runMediumSync(FunctionalTester $I)
-    {
-        $I->wantTo('Run medium synchronization task.');
-
-        $I->amOnPage('/admin/sync/medium');
-        $I->seeInSource('Sync Task Output');
-    }
-
-    /**
-     * @before setupComplete
-     * @before login
-     */
-    public function runLongSync(FunctionalTester $I)
-    {
-        $I->wantTo('Run long synchronization task.');
-
-        $I->amOnPage('/admin/sync/long');
-        $I->seeInSource('Sync Task Output');
     }
 }

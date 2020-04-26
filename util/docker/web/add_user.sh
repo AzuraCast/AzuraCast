6046746a3a7ed84beed626292b06cb76d81f632b
Buster "Silver Eagle" Neece <buster@busterneece.com>
2020-04-25 21:48:14 -0500

    #2740 -- Workaround sudo issue in Ubuntu 20.04.

--- util/docker/web/add_user.sh
+++ util/docker/web/add_user.sh
@@ -5,6 +5,9 @@ set -x
 
 $minimal_apt_get_install sudo
 
+# Workaround for sudo errors in containers, see: https://github.com/sudo-project/sudo/issues/42
+echo "Set disable_coredump false" >> /etc/sudo.conf
+
 adduser --home /var/azuracast --disabled-password --gecos "" azuracast
 
 usermod -aG docker_env azuracast

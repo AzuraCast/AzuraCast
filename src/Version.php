c45638de7dfd04c263b4a895d2303ea1495864ab
Buster "Silver Eagle" Neece <buster@busterneece.com>
2020-05-06 19:03:18 -0500

    Update version to 0.10.2.

--- src/Version.php
+++ src/Version.php
@@ -12,7 +12,7 @@ use Symfony\Component\Process\Process;
 class Version
 {
     /** @var string Version that is displayed if no Git repository information is present. */
-    public const FALLBACK_VERSION = '0.10.1';
+    public const FALLBACK_VERSION = '0.10.2';
 
     protected CacheInterface $cache;
 

92dae59db889d149a46a1b26a4ad938d46a9dc10
Buster "Silver Eagle" Neece <buster@busterneece.com>
2020-05-14 20:17:30 -0500

    Create unified AudioPlayer Vue component.

--- frontend/vue/InlinePlayer.vue
+++ frontend/vue/InlinePlayer.vue
@@ -1,40 +1,42 @@
 <template>
-    <div class="ml-3 player-inline" v-if="isPlaying">
-        <audio ref="player"/>
+    <div>
+        <audio-player ref="player"></audio-player>
 
-        <div class="inline-seek d-inline-flex align-items-center ml-1" v-if="duration !== 0">
-            <div class="flex-shrink-0 mx-1 text-muted time-display">
-                {{ currentTimeText }}
+        <div class="ml-3 player-inline" v-if="is_playing">
+            <div class="inline-seek d-inline-flex align-items-center ml-1" v-if="duration !== 0">
+                <div class="flex-shrink-0 mx-1 text-muted time-display">
+                    {{ currentTimeText }}
+                </div>
+                <div class="flex-fill mx-2">
+                    <input type="range" :title="langSeek" class="player-seek-range custom-range" min="0" max="100"
+                           step="1" v-model="progress">
+                </div>
+                <div class="flex-shrink-0 mx-1 text-muted time-display">
+                    {{ durationText }}
+                </div>
             </div>
-            <div class="flex-fill mx-2">
-                <input type="range" :title="langSeek" class="player-seek-range custom-range" min="0" max="100"
-                       step="1" v-model="progress">
-            </div>
-            <div class="flex-shrink-0 mx-1 text-muted time-display">
-                {{ durationText }}
-            </div>
-        </div>
 
-        <a class="btn btn-sm btn-outline-light px-2 ml-1" href="#" @click.prevent="stop()">
-            <i class="material-icons" aria-hidden="true">pause</i>
-            <span class="sr-only" v-translate>Pause</span>
-        </a>
-        <div class="inline-volume-controls d-inline-flex align-items-center ml-1">
-            <div class="flex-shrink-0">
-                <a class="btn btn-sm btn-outline-light px-2" href="#" @click.prevent="volume = 0">
-                    <i class="material-icons" aria-hidden="true">volume_mute</i>
-                    <span class="sr-only" v-translate>Mute</span>
-                </a>
-            </div>
-            <div class="flex-fill mx-1">
-                <input type="range" :title="langVolume" class="player-volume-range custom-range" min="0" max="100"
-                       step="1" v-model="volume">
-            </div>
-            <div class="flex-shrink-0">
-                <a class="btn btn-sm btn-outline-light px-2" href="#" @click.prevent="volume = 100">
-                    <i class="material-icons" aria-hidden="true">volume_up</i>
-                    <span class="sr-only" v-translate>Full Volume</span>
-                </a>
+            <a class="btn btn-sm btn-outline-light px-2 ml-1" href="#" @click.prevent="stop()">
+                <i class="material-icons" aria-hidden="true">pause</i>
+                <span class="sr-only" v-translate>Pause</span>
+            </a>
+            <div class="inline-volume-controls d-inline-flex align-items-center ml-1">
+                <div class="flex-shrink-0">
+                    <a class="btn btn-sm btn-outline-light px-2" href="#" @click.prevent="volume = 0">
+                        <i class="material-icons" aria-hidden="true">volume_mute</i>
+                        <span class="sr-only" v-translate>Mute</span>
+                    </a>
+                </div>
+                <div class="flex-fill mx-1">
+                    <input type="range" :title="langVolume" class="player-volume-range custom-range" min="0" max="100"
+                           step="1" v-model="volume">
+                </div>
+                <div class="flex-shrink-0">
+                    <a class="btn btn-sm btn-outline-light px-2" href="#" @click.prevent="volume = 100">
+                        <i class="material-icons" aria-hidden="true">volume_up</i>
+                        <span class="sr-only" v-translate>Full Volume</span>
+                    </a>
+                </div>
             </div>
         </div>
     </div>
@@ -63,42 +65,17 @@
 </style>
 
 <script>
-    import store from 'store';
-    import getLogarithmicVolume from './inc/logarithmic_volume';
+    import AudioPlayer from './components/AudioPlayer';
 
     export default {
+        components: { AudioPlayer },
         data () {
             return {
-                'isPlaying': false,
-                'volume': 55,
-                'audio': null,
-                'duration': 0,
-                'currentTime': 0
+                is_mounted: false
             };
         },
-        created () {
-            // Allow pausing from the mobile metadata update.
-            if ('mediaSession' in navigator) {
-                navigator.mediaSession.setActionHandler('pause', () => {
-                    this.stop();
-                });
-            }
-
-            // Check webstorage for existing volume preference.
-            if (store.enabled && store.get('player_volume') !== undefined) {
-                this.volume = store.get('player_volume', this.volume);
-            }
-
-            this.$eventHub.$on('player_toggle', (url) => {
-                if (this.isPlaying && this.audio.src === url) {
-                    this.stop();
-                } else {
-                    this.stop();
-                    Vue.nextTick(() => {
-                        this.play(url);
-                    });
-                }
-            });
+        mounted () {
+            this.is_mounted = true;
         },
         computed: {
             langSeek () {
@@ -109,87 +86,71 @@
             },
             durationText () {
                 let minutes = Math.floor(this.duration / 60),
-                        seconds_int = this.duration - minutes * 60,
-                        seconds_str = seconds_int.toString(),
-                        seconds = seconds_str.substr(0, 2);
+                    seconds_int = this.duration - minutes * 60,
+                    seconds_str = seconds_int.toString(),
+                    seconds = seconds_str.substr(0, 2);
 
                 return minutes + ':' + seconds;
             },
             currentTimeText () {
                 let current_minute = parseInt(this.currentTime / 60) % 60,
-                        current_seconds_long = this.currentTime % 60,
-                        current_seconds = current_seconds_long.toFixed();
+                    current_seconds_long = this.currentTime % 60,
+                    current_seconds = current_seconds_long.toFixed();
 
                 return (current_minute < 10 ? '0' + current_minute : current_minute) + ':' + (current_seconds < 10 ? '0' + current_seconds : current_seconds);
             },
-            progress: {
-                get () {
-                    return (this.duration !== 0) ? Math.round((this.currentTime / this.duration) * 100, 2) : 0;
-                },
-                set (progress) {
-                    if (this.audio !== null) {
-                        this.audio.currentTime = (progress / 100) * this.duration;
-                    }
-                }
-            }
-        },
-        watch: {
-            volume (volume) {
-                if (this.audio !== null) {
-                    this.audio.volume = getLogarithmicVolume(volume);
+            duration () {
+                if (!this.is_mounted) {
+                    return;
                 }
 
-                if (store.enabled) {
-                    store.set('player_volume', volume);
+                return this.$refs.player.getDuration();
+            },
+            currentTime () {
+                if (!this.is_mounted) {
+                    return;
+                }
+
+                return this.$refs.player.getCurrentTime();
+            },
+            is_playing () {
+                if (!this.is_mounted) {
+                    return;
+                }
+
+                return this.$refs.player.isPlaying();
+            },
+            volume: {
+                get () {
+                    if (!this.is_mounted) {
+                        return;
+                    }
+
+                    return this.$refs.player.getVolume();
+                },
+                set (vol) {
+                    this.$refs.player.setVolume(vol);
+                }
+            },
+            progress: {
+                get () {
+                    if (!this.is_mounted) {
+                        return;
+                    }
+
+                    return this.$refs.player.getProgress();
+                },
+                set (progress) {
+                    this.$refs.player.setProgress(progress);
                 }
             }
         },
         methods: {
             play (url) {
-                if (this.isPlaying) {
-                    this.stop();
-                    Vue.nextTick(() => {
-                        this.play(url);
-                    });
-                }
-
-                this.isPlaying = true;
-
-                Vue.nextTick(() => {
-                    this.audio = this.$refs.player;
-
-                    this.audio.onended = () => {
-                        this.stop();
-                    };
-
-                    this.audio.ontimeupdate = () => {
-                        this.duration = (this.audio.duration !== Infinity && !isNaN(this.audio.duration)) ? this.audio.duration : 0;
-                        this.currentTime = this.audio.currentTime;
-                    };
-
-                    this.audio.volume = getLogarithmicVolume(this.volume);
-
-                    this.audio.src = url;
-
-                    this.audio.load();
-                    this.audio.play();
-                });
-
-                this.$eventHub.$emit('player_playing', url);
+                this.$refs.player.play(url);
             },
             stop () {
-                if (!this.isPlaying) {
-                    return;
-                }
-
-                this.$eventHub.$emit('player_stopped', this.audio.src);
-
-                this.audio.pause();
-                this.audio.src = '';
-
-                this.duration = 0;
-                this.currentTime = 0;
-                this.isPlaying = false;
+                this.$refs.player.stop();
             }
         }
     };

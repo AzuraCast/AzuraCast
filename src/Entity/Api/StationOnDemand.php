c90a257087fb70c29d6c1e61f841cb8980201eb8
Buster "Silver Eagle" Neece <buster@busterneece.com>
2020-05-14 20:18:17 -0500

    Create API endpoints for OnDemand stream/downloads.

---
+++ src/Entity/Api/StationOnDemand.php
@@ -0,0 +1,45 @@
+<?php
+namespace App\Entity\Api;
+
+use OpenApi\Annotations as OA;
+use Psr\Http\Message\UriInterface;
+
+/**
+ * @OA\Schema(type="object", schema="Api_StationOnDemand")
+ */
+class StationOnDemand implements ResolvableUrlInterface
+{
+    /**
+     * Track ID unique identifier
+     *
+     * @OA\Property(example=1)
+     * @var string
+     */
+    public string $track_id;
+
+    /**
+     * URL to download/play track.
+     *
+     * @OA\Property(example="/api/station/1/ondemand/download/1")
+     * @var string
+     */
+    public string $download_url;
+
+    /**
+     * Song
+     *
+     * @OA\Property
+     * @var Song
+     */
+    public Song $song;
+
+    /**
+     * Re-resolve any Uri instances to reflect base URL changes.
+     *
+     * @param UriInterface $base
+     */
+    public function resolveUrls(UriInterface $base): void
+    {
+        $this->song->resolveUrls($base);
+    }
+}

e62e03c3bd0829844d52abd2d818770c5a54937a
Bjarn Bronsveld <14638441+bjarn@users.noreply.github.com>
2020-08-25 21:32:44 +0200

    Update Version20200825183243.php

--- src/Entity/Migration/Version20200825183243.php
+++ src/Entity/Migration/Version20200825183243.php
@@ -20,7 +20,7 @@ final class Version20200825183243 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE station_media MODIFY `length` NUMERIC(3, 1)');
+        $this->addSql('ALTER TABLE station_media MODIFY `length` NUMERIC(7, 1)');
     }
 
     public function down(Schema $schema) : void

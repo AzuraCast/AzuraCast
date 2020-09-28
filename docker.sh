2edcde5aebf0bd506eed11b3b46a4df69d18846d
Buster "Silver Eagle" Neece <buster@busterneece.com>
2020-09-28 18:04:49 -0500

    Persist Composer vendor directory to make plugins work again.

--- docker.sh
+++ docker.sh
@@ -369,6 +369,7 @@ update() {
             docker-compose down
         fi
 
+        docker volume rm azuracast_www_vendor
         docker volume rm azuracast_tmp_data
         docker volume rm azuracast_redis_data
 

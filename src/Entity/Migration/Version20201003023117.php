ea19b9722f240a853a1044c54ebbecfb87cb85a1
Buster "Silver Eagle" Neece <buster@busterneece.com>
2020-10-07 03:50:12 -0500

    #3240 -- Soften migration to handle erroneous migration states.

--- src/Entity/Migration/Version20201003023117.php
+++ src/Entity/Migration/Version20201003023117.php
@@ -20,14 +20,14 @@ final class Version20201003023117 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE song_history DROP FOREIGN KEY FK_2AD16164A0BDB2F3');
-        $this->addSql('DROP INDEX IDX_2AD16164A0BDB2F3 ON song_history');
+        $this->addSql('ALTER TABLE song_history DROP FOREIGN KEY IF EXISTS FK_2AD16164A0BDB2F3');
+        $this->addSql('DROP INDEX IF EXISTS IDX_2AD16164A0BDB2F3 ON song_history');
 
-        $this->addSql('ALTER TABLE station_media DROP FOREIGN KEY FK_32AADE3AA0BDB2F3');
-        $this->addSql('DROP INDEX IDX_32AADE3AA0BDB2F3 ON station_media');
+        $this->addSql('ALTER TABLE station_media DROP FOREIGN KEY IF EXISTS FK_32AADE3AA0BDB2F3');
+        $this->addSql('DROP INDEX IF EXISTS IDX_32AADE3AA0BDB2F3 ON station_media');
 
-        $this->addSql('ALTER TABLE station_queue DROP FOREIGN KEY FK_277B0055A0BDB2F3');
-        $this->addSql('DROP INDEX IDX_277B0055A0BDB2F3 ON station_queue');
+        $this->addSql('ALTER TABLE station_queue DROP FOREIGN KEY IF EXISTS FK_277B0055A0BDB2F3');
+        $this->addSql('DROP INDEX IF EXISTS IDX_277B0055A0BDB2F3 ON station_queue');
 
         $this->addSql('DROP TABLE songs');
 
@@ -36,7 +36,7 @@ final class Version20201003023117 extends AbstractMigration
 
     public function down(Schema $schema): void
     {
-        $this->addSql('ALTER TABLE station_media DROP CHANGE song_id song_id VARCHAR(50) CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_general_ci`');
+        $this->addSql('ALTER TABLE station_media CHANGE song_id song_id VARCHAR(50) CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_general_ci`');
 
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE songs (id VARCHAR(50) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_general_ci`, text VARCHAR(150) CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_general_ci`, artist VARCHAR(150) CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_general_ci`, title VARCHAR(150) CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_general_ci`, created INT NOT NULL, play_count INT NOT NULL, last_played INT NOT NULL, INDEX search_idx (text, artist, title), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');

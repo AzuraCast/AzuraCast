c65e6867983dd33179c617fa608ca70d3386c50e
Buster "Silver Eagle" Neece <buster@busterneece.com>
2020-10-09 04:38:17 -0500

    Add initial support for Greek and Swedish localizations.

--- config/settings.php
+++ config/settings.php
@@ -21,6 +21,7 @@ return [
             'de_DE.UTF-8' => 'Deutsch',             // German
             'es_ES.UTF-8' => 'Español',             // Spanish
             'fr_FR.UTF-8' => 'Français',            // French
+            'el_GR.UTF-8' => 'ελληνικά',            // Greek
             'it_IT.UTF-8' => 'Italiano',            // Italian
             'hu_HU.UTF-8' => 'magyar',              // Hungarian
             'nl_NL.UTF-8' => 'Nederlands',          // Dutch
@@ -28,7 +29,7 @@ return [
             'pt_PT.UTF-8' => 'Português',           // Portuguese
             'pt_BR.UTF-8' => 'Português do Brasil', // Brazilian Portuguese
             'ru_RU.UTF-8' => 'Русский язык',        // Russian
-            // 'sv_SE.UTF-8' => 'Svenska',          // Swedish
+            'sv_SE.UTF-8' => 'Svenska',             // Swedish
             'tr_TR.UTF-8' => 'Türkçe',              // Turkish
         ],
     ],

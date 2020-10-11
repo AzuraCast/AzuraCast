5b626b02e38c77b0caaab2e4161c21505360c7b9
xewl <kenverhaegen@gmail.com>
2020-10-11 20:42:59 +0200

    Add strict types & getDescription as newer Doctrine versions

--- src/Entity/Migration/Version20201010170333.php
+++ src/Entity/Migration/Version20201010170333.php
@@ -1,25 +1,24 @@
 <?php
+
+declare(strict_types=1);
+
 namespace App\Entity\Migration;
 
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20201010170333 extends AbstractMigration
 {
-    /**
-     * @param Schema $schema
-     */
+    public function getDescription(): string
+    {
+        return 'Add genre field as a first-class meta field for Station Media.';
+    }
+    
     public function up(Schema $schema): void
     {
         $this->addSql('ALTER TABLE station_media ADD genre varchar(30) COLLATE utf8_unicode_ci DEFAULT NULL');
     }
-
-    /**
-     * @param Schema $schema
-     */
+    
     public function down(Schema $schema): void
     {
         $this->addSql('ALTER TABLE station_media DROP genre');

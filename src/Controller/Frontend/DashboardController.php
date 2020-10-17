7f6352e61d545cd381888d0574d2db783e163da9
Buster "Silver Eagle" Neece <buster@busterneece.com>
2020-10-17 08:12:02 -0500

    Add ability to view unique listener totals from dashboard chart.

--- src/Controller/Frontend/DashboardController.php
+++ src/Controller/Frontend/DashboardController.php
@@ -14,7 +14,6 @@ use Carbon\CarbonImmutable;
 use Doctrine\ORM\EntityManagerInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\SimpleCache\CacheInterface;
-use stdClass;
 
 class DashboardController
 {
@@ -150,89 +149,134 @@ class DashboardController
     }
 
     /**
+     * @param array $stationsToView
+     * @param bool $showAdmin
+     *
      * @return mixed[]
      */
-    protected function getMetrics(array $view_stations, bool $show_admin = false): array
+    protected function getMetrics(array $stationsToView, bool $showAdmin = false): array
     {
         // Statistics by day.
-        $station_averages = [];
-
         $threshold = CarbonImmutable::parse('-180 days');
 
         $stats = $this->em->createQuery(/** @lang DQL */ 'SELECT a.station_id, a.moment, a.number_avg, a.number_unique
             FROM App\Entity\Analytics a
-            WHERE (a.station_id IS NULL OR a.station_id IN (:stations))
+            WHERE a.station_id IN (:stations)
             AND a.type = :type
             AND a.moment >= :threshold')
-            ->setParameter('stations', $view_stations)
+            ->setParameter('stations', $stationsToView)
             ->setParameter('type', Entity\Analytics::INTERVAL_DAILY)
             ->setParameter('threshold', $threshold)
             ->getArrayResult();
 
+        $stationStats = [
+            'average' => [],
+            'unique' => [],
+        ];
+
+        $showAllStations = $showAdmin && count($stationsToView) > 1;
+
         foreach ($stats as $row) {
-            $station_id = $row['station_id'] ?? 'all';
+            $stationId = $row['station_id'];
 
             /** @var CarbonImmutable $moment */
             $moment = $row['moment'];
 
-            $moment = $moment->getTimestamp() * 1000;
+            $sortableKey = $moment->format('Y-m-d');
+            $jsTimestamp = $moment->getTimestamp() * 1000;
 
-            $station_averages[$station_id][$moment] = [
-                $moment,
-                round($row['number_avg'], 2),
+            $average = round($row['number_avg'], 2);
+            $unique = $row['number_unique'];
+
+            $stationStats['average'][$stationId][$sortableKey] = [
+                $jsTimestamp,
+                $average,
             ];
-        }
 
-        $metric_stations = [];
-        if ($show_admin && count($view_stations) > 1) {
-            $metric_stations['all'] = __('All Stations');
-        }
-        foreach ($view_stations as $station_id => $station_info) {
-            $metric_stations[$station_id] = $station_info['station']['name'];
-        }
+            if (null !== $unique) {
+                $stationStats['unique'][$stationId][$sortableKey] = [
+                    $jsTimestamp,
+                    $unique,
+                ];
+            }
 
-        $station_metrics = [];
-        $station_metrics_alt = [];
-
-        foreach ($metric_stations as $station_id => $station_name) {
-            if (isset($station_averages[$station_id])) {
-                $series_obj = new stdClass();
-                $series_obj->label = $station_name;
-                $series_obj->type = 'line';
-                $series_obj->fill = false;
-
-                $station_metrics_alt[] = '<p>' . $series_obj->label . '</p>';
-                $station_metrics_alt[] = '<dl>';
-
-                ksort($station_averages[$station_id]);
-
-                $series_data = [];
-                foreach ($station_averages[$station_id] as $serie) {
-                    $series_row = new stdClass();
-                    $series_row->t = $serie[0];
-                    $series_row->y = $serie[1];
-                    $series_data[] = $series_row;
-
-                    $serie_date = gmdate('Y-m-d', $serie[0] / 1000);
-                    $station_metrics_alt[] = sprintf(
-                        '<dt><time data-original="%s">%s</time></dt>',
-                        $serie[0],
-                        $serie_date
-                    );
-                    $station_metrics_alt[] = '<dd>' . $serie[1] . ' ' . __('Listeners') . '</dd>';
+            if ($showAllStations) {
+                if (!isset($stationStats['average']['all'][$sortableKey])) {
+                    $stationStats['average']['all'][$sortableKey] = [
+                        $jsTimestamp,
+                        0,
+                    ];
                 }
+                $stationStats['average']['all'][$sortableKey][1] += $average;
 
-                $station_metrics_alt[] = '</dl>';
-
-                $series_obj->data = $series_data;
-
-                $station_metrics[] = $series_obj;
+                if (null !== $unique) {
+                    if (!isset($stationStats['unique']['all'][$sortableKey])) {
+                        $stationStats['unique']['all'][$sortableKey] = [
+                            $jsTimestamp,
+                            0,
+                        ];
+                    }
+                    $stationStats['unique']['all'][$sortableKey][1] += $unique;
+                }
             }
         }
 
-        return [
-            'station' => json_encode($station_metrics, JSON_THROW_ON_ERROR),
-            'station_alt' => implode('', $station_metrics_alt),
+        $stationsInMetric = [];
+        if ($showAllStations) {
+            $stationsInMetric['all'] = __('All Stations');
+        }
+        foreach ($stationsToView as $stationId => $station_info) {
+            $stationsInMetric[$stationId] = $station_info['station']['name'];
+        }
+
+        $jsStats = [
+            'average' => [
+                'metrics' => [],
+                'alt' => '',
+            ],
+            'unique' => [
+                'metrics' => [],
+                'alt' => '',
+            ],
         ];
+
+        foreach ($stationsInMetric as $stationId => $stationName) {
+            foreach ($stationStats as $statKey => $statRows) {
+                if (!isset($statRows[$stationId])) {
+                    continue;
+                }
+
+                $series = [
+                    'label' => $stationName,
+                    'type' => 'line',
+                    'fill' => false,
+                    'data' => [],
+                ];
+
+                $jsStats[$statKey]['alt'] .= '<p>' . $stationName . '</p>';
+                $jsStats[$statKey]['alt'] .= '<dl>';
+
+                ksort($statRows[$stationId]);
+
+                foreach ($statRows[$stationId] as $sortableKey => [$jsTimestamp, $value]) {
+                    $series['data'][] = [
+                        't' => $jsTimestamp,
+                        'y' => $value,
+                    ];
+
+                    $jsStats[$statKey]['alt'] .= sprintf(
+                        '<dt><time data-original="%s">%s</time></dt>',
+                        $jsTimestamp,
+                        $sortableKey
+                    );
+                    $jsStats[$statKey]['alt'] .= '<dd>' . $value . ' ' . __('Listeners') . '</dd>';
+                }
+
+                $jsStats[$statKey]['alt'] .= '</dl>';
+                $jsStats[$statKey]['metrics'][] = $series;
+            }
+        }
+
+        return $jsStats;
     }
 }

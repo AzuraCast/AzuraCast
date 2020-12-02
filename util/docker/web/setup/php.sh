749b12638f5be6b6ffedcd7207c724cfa497d3d5
Buster "Silver Eagle" Neece <buster@busterneece.com>
2020-12-01 18:44:39 -0600

    Revert accidental PHP 8.0 early upgrade.

--- util/docker/web/setup/php.sh
+++ util/docker/web/setup/php.sh
@@ -6,17 +6,17 @@ set -x
 add-apt-repository -y ppa:ondrej/php
 apt-get update
 
-$minimal_apt_get_install php8.0-fpm php8.0-cli php8.0-gd \
-    php8.0-curl php8.0-xml php8.0-zip php8.0-bcmath php8.0-gmp \
-    php8.0-mysqlnd php8.0-mbstring php8.0-intl php8.0-redis \
+$minimal_apt_get_install php7.4-fpm php7.4-cli php7.4-gd \
+    php7.4-curl php7.4-xml php7.4-zip php7.4-bcmath php7.4-gmp \
+    php7.4-mysqlnd php7.4-mbstring php7.4-intl php7.4-redis \
     mariadb-client
 
 # Copy PHP configuration
 mkdir -p /run/php
-touch /run/php/php8.0-fpm.pid
+touch /run/php/php7.4-fpm.pid
 
-cp /bd_build/php/php.ini.tmpl /etc/php/8.0/fpm/05-azuracast.ini.tmpl
-cp /bd_build/php/www.conf.tmpl /etc/php/8.0/fpm/www.conf.tmpl
+cp /bd_build/php/php.ini.tmpl /etc/php/7.4/fpm/05-azuracast.ini.tmpl
+cp /bd_build/php/www.conf.tmpl /etc/php/7.4/fpm/www.conf.tmpl
 
 # Install Composer
 curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/bin --filename=composer

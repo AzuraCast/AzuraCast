8f900af32feaac426faaf37a6831f50a0ecbfd89
Vaalyn <vaalyndev@gmail.com>
2021-04-18 03:50:54 +0200

    Add PHP-SPX profiling extension for development environment (#4032)
    Co-authored-by: Buster "Silver Eagle" Neece <buster@busterneece.com>

--- util/docker/web/setup/php.sh
+++ util/docker/web/setup/php.sh
@@ -9,7 +9,7 @@ apt-get update
 $minimal_apt_get_install php7.4-fpm php7.4-cli php7.4-gd \
     php7.4-curl php7.4-xml php7.4-zip php7.4-bcmath php7.4-gmp \
     php7.4-mysqlnd php7.4-mbstring php7.4-intl php7.4-redis \
-    php7.4-maxminddb \
+    php7.4-maxminddb php7.4-xdebug \
     mariadb-client
 
 # Copy PHP configuration
@@ -21,3 +21,19 @@ cp /bd_build/php/www.conf.tmpl /etc/php/7.4/fpm/www.conf.tmpl
 
 # Install Composer
 curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/bin --filename=composer
+
+# Install PHP SPX profiler
+$minimal_apt_get_install php7.4-dev zlib1g-dev build-essential
+
+cd /bd_build
+git clone https://github.com/NoiseByNorthwest/php-spx.git
+cd php-spx
+phpize
+./configure
+make
+sudo make install
+
+apt-get remove --purge -y php7.4-dev zlib1g-dev build-essential
+
+echo "extension=spx.so" > /etc/php/7.4/cli/conf.d/30-spx.ini
+echo "extension=spx.so" > /etc/php/7.4/fpm/conf.d/30-spx.ini

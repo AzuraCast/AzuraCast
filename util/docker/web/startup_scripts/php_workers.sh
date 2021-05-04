642ca15803ea0ed506e6933bac353dcb33a5a6af
Buster "Silver Eagle" Neece <buster@busterneece.com>
2021-05-03 23:35:09 -0500

    Temporarily disable multiple queue workers; explanation in code.

--- util/docker/web/startup_scripts/php_workers.sh
+++ util/docker/web/startup_scripts/php_workers.sh
@@ -2,6 +2,16 @@
 
 # Duplicate php-worker runit script based on environment variable
 
+echo "Additional workers currently disabled."
+exit 0
+# Temporarily disabled. Explanation:
+# The current worker setup uses MariaDB as the underlying message queue implementation. Multiple workers on the same
+# queues causes multiple process to have write locks on the same table of the same database at the same time, which
+# can tend to create a condition where the processes "pile up" on each other, causing semaphore lock overflow issues
+# that can bring down an installation rather easily.
+#
+# Disabling this script forces a single worker to run to process message queues.
+
 echo "Adding $ADDITIONAL_MEDIA_SYNC_WORKER_COUNT additional workers"
 
 for ((WORKER_NUMBER = 1; WORKER_NUMBER <= ADDITIONAL_MEDIA_SYNC_WORKER_COUNT; WORKER_NUMBER++)); do

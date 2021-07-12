be3aacd5c3bc8c6506414747de369ce6ba35479a
Vaalyn <vaalyndev@gmail.com>
2021-07-12 02:02:06 +0200

    Update Liquidsoap to match version from Docker installations

--- update.sh
+++ update.sh
@@ -34,7 +34,7 @@ else
 fi
 
 APP_ENV="${APP_ENV:-production}"
-UPDATE_REVISION="${UPDATE_REVISION:-63}"
+UPDATE_REVISION="${UPDATE_REVISION:-64}"
 
 echo "Updating AzuraCast (Environment: $APP_ENV, Update revision: $UPDATE_REVISION)"
 

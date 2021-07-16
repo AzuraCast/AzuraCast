acebfeed2d20a2296d698838e43891b4f3df0087
Buster "Silver Eagle" Neece <buster@busterneece.com>
2021-07-15 23:06:42 -0500

    Fixture fix.

--- src/Entity/Fixture/Settings.php
+++ src/Entity/Fixture/Settings.php
@@ -17,9 +17,9 @@ class Settings extends AbstractFixture
         }
 
         $settings = new Entity\Settings();
-        $settings->setBaseUrl(getenv('INIT_BASE_URL') ?? 'docker.local');
-        $settings->setInstanceName(getenv('INIT_INSTANCE_NAME') ?? 'local test');
-        $settings->setGeoliteLicenseKey(getenv('INIT_GEOLITE_LICENSE_KEY') ?? '');
+        $settings->setBaseUrl((string)(getenv('INIT_BASE_URL') ?? 'docker.local'));
+        $settings->setInstanceName((string)(getenv('INIT_INSTANCE_NAME') ?? 'local test'));
+        $settings->setGeoliteLicenseKey((string)(getenv('INIT_GEOLITE_LICENSE_KEY') ?? ''));
 
         $settings->setSetupCompleteTime(time());
         $settings->setPreferBrowserUrl(true);

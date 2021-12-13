527b9c4f865038e08990dc61308ce27ae7d6dac3
Buster "Silver Eagle" Neece <buster@busterneece.com>
2021-12-12 22:11:55 -0600

    Extend beanstalkd timeout.

--- util/docker/web/setup/beanstalkd.sh
+++ util/docker/web/setup/beanstalkd.sh
@@ -8,4 +8,6 @@ apt-get update
 # Prevent systemd auto-startup
 ln -s /dev/null /etc/systemd/system/beanstalkd.service
 
-$minimal_apt_get_install beanstalkd
+echo "STARTTIME=30" > /etc/default/beanstalkd
+
+$minimal_apt_get_install -o Dpkg::Options::="--force-confdef" beanstalkd

866779bb65733528f176c052c7150b49ac4c12e2
hcbd <hcbd@users.noreply.github.com>
2021-12-18 01:48:37 +0100

    Feature: Playbutton in playlist re-order modal

--- frontend/vue/components/Stations/Playlists/ReorderModal.vue
+++ frontend/vue/components/Stations/Playlists/ReorderModal.vue
@@ -1,18 +1,30 @@
 <template>
     <b-modal size="lg" id="reorder_modal" ref="modal" :title="langTitle" :busy="loading" hide-footer>
         <b-overlay variant="card" :show="loading">
+            <div style="min-height: 40px;" class="flex-fill text-left bg-primary rounded">
+              <inline-player ref="player"></inline-player>
+            </div>
             <b-table-simple striped class="sortable mb-0">
                 <b-thead>
                     <tr>
-                        <th style="width: 30%;" key="lang_col_title" v-translate>Title</th>
-                        <th style="width: 25%;" key="lang_col_artist" v-translate>Artist</th>
-                        <th style="width: 25%;" key="lang_col_album" v-translate>Album</th>
+                        <th style="width: 34%;" key="lang_col_title" v-translate>Title</th>
+                        <th style="width: 23%;" key="lang_col_artist" v-translate>Artist</th>
+                        <th style="width: 23%;" key="lang_col_album" v-translate>Album</th>
                         <th style="width: 20%;" key="lang_col_actions" v-translate>Actions</th>
                     </tr>
                 </b-thead>
                 <draggable v-model="media" tag="tbody" @change="save">
                     <tr class="align-middle" v-for="(row,index) in media" :key="media.id">
-                        <td><big>{{ row.media.title }}</big></td>
+                        <td>
+                            <div style="display: flex; align-items: center; justify-content: flex-start;">
+                                <div style="font-size: 1.1rem; margin-right: 1rem;">
+                                    <play-button :url="row.playback_url" icon-class="outlined"></play-button>
+                                </div>
+                                <div class="flex-fill">
+                                    <big>{{ row.media.title }}</big>
+                                </div>
+                            </div>
+                        </td>
                         <td>{{ row.media.artist }}</td>
                         <td>{{ row.media.album }}</td>
                         <td>
@@ -43,12 +55,16 @@ table.sortable {
 <script>
 import Draggable from 'vuedraggable';
 import Icon from '~/components/Common/Icon';
+import PlayButton from "~/components/Common/PlayButton";
+import InlinePlayer from '../../InlinePlayer';
 
 export default {
     name: 'ReorderModal',
     components: {
         Icon,
-        Draggable
+        Draggable,
+        PlayButton,
+        InlinePlayer
     },
     data() {
         return {
@@ -75,7 +91,27 @@ export default {
             this.loading = true;
 
             this.axios.get(this.reorderUrl).then((resp) => {
-                this.media = resp.data;
+                // this is a bit of a hack to get the api_url & station_id; suggetions welcome.
+                let playbackBaseUrl = "";
+                if (this.reorderUrl.includes("://")) {
+                    playbackBaseUrl = this.reorderUrl.match(/.*\/station\/(\d+)\//i)?.[0] || "";
+                } else {
+                    playbackBaseUrl = 
+                      window.location.protocol 
+                      + "//" 
+                      + window.location.host 
+                      + this.reorderUrl.match(/.*\/station\/(\d+)\//i)?.[0] || ""; // selects all after domain return "/api/station/{id}/" or ""
+                }
+                playbackBaseUrl += "files/play/";
+
+                // add playback url to each media object
+                this.media = resp.data.map( obj => {
+                  const playbackUrl = playbackBaseUrl + obj.media_id;
+                  return {
+                    ...obj, 
+                    playback_url: playbackUrl
+                  }
+                });
                 this.loading = false;
             });
         },

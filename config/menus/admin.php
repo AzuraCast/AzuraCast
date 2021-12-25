c2df37b66063c576cda9b0050a615461115e069a
Buster "Silver Eagle" Neece <buster@busterneece.com>
2021-12-25 08:32:10 -0600

    Reformat menu files; remove "Beta" on Podcasts.

--- config/menus/admin.php
+++ config/menus/admin.php
@@ -13,93 +13,93 @@ return function (App\Event\BuildAdminMenu $e) {
         [
             'maintenance' => [
                 'label' => __('System Maintenance'),
-                'icon' => 'router',
+                'icon'  => 'router',
                 'items' => [
-                    'settings' => [
-                        'label' => __('System Settings'),
-                        'url' => (string)$router->named('admin:settings:index'),
+                    'settings'          => [
+                        'label'      => __('System Settings'),
+                        'url'        => (string)$router->named('admin:settings:index'),
                         'permission' => Acl::GLOBAL_SETTINGS,
                     ],
-                    'branding' => [
-                        'label' => __('Custom Branding'),
-                        'url' => (string)$router->named('admin:branding:index'),
+                    'branding'          => [
+                        'label'      => __('Custom Branding'),
+                        'url'        => (string)$router->named('admin:branding:index'),
                         'permission' => Acl::GLOBAL_SETTINGS,
                     ],
-                    'logs' => [
-                        'label' => __('System Logs'),
-                        'url' => (string)$router->named('admin:logs:index'),
+                    'logs'              => [
+                        'label'      => __('System Logs'),
+                        'url'        => (string)$router->named('admin:logs:index'),
                         'permission' => Acl::GLOBAL_LOGS,
                     ],
                     'storage_locations' => [
-                        'label' => __('Storage Locations'),
-                        'url' => (string)$router->named('admin:storage_locations:index'),
+                        'label'      => __('Storage Locations'),
+                        'url'        => (string)$router->named('admin:storage_locations:index'),
                         'permission' => Acl::GLOBAL_STORAGE_LOCATIONS,
                     ],
-                    'backups' => [
-                        'label' => __('Backups'),
-                        'url' => (string)$router->named('admin:backups:index'),
+                    'backups'           => [
+                        'label'      => __('Backups'),
+                        'url'        => (string)$router->named('admin:backups:index'),
                         'permission' => Acl::GLOBAL_BACKUPS,
                     ],
-                    'debug' => [
-                        'label' => __('System Debugger'),
-                        'url' => (string)$router->named('admin:debug:index'),
+                    'debug'             => [
+                        'label'      => __('System Debugger'),
+                        'url'        => (string)$router->named('admin:debug:index'),
                         'permission' => Acl::GLOBAL_ALL,
                     ],
                 ],
             ],
-            'users' => [
+            'users'       => [
                 'label' => __('Users'),
-                'icon' => 'group',
+                'icon'  => 'group',
                 'items' => [
                     'manage_users' => [
-                        'label' => __('User Accounts'),
-                        'url' => (string)$router->named('admin:users:index'),
+                        'label'      => __('User Accounts'),
+                        'url'        => (string)$router->named('admin:users:index'),
                         'permission' => Acl::GLOBAL_ALL,
                     ],
-                    'permissions' => [
-                        'label' => __('Roles & Permissions'),
-                        'url' => (string)$router->named('admin:permissions:index'),
+                    'permissions'  => [
+                        'label'      => __('Roles & Permissions'),
+                        'url'        => (string)$router->named('admin:permissions:index'),
                         'permission' => Acl::GLOBAL_ALL,
                     ],
-                    'auditlog' => [
-                        'label' => __('Audit Log'),
-                        'url' => (string)$router->named('admin:auditlog:index'),
+                    'auditlog'     => [
+                        'label'      => __('Audit Log'),
+                        'url'        => (string)$router->named('admin:auditlog:index'),
                         'permission' => Acl::GLOBAL_LOGS,
                     ],
-                    'api_keys' => [
-                        'label' => __('API Keys'),
-                        'url' => (string)$router->named('admin:api:index'),
+                    'api_keys'     => [
+                        'label'      => __('API Keys'),
+                        'url'        => (string)$router->named('admin:api:index'),
                         'permission' => Acl::GLOBAL_API_KEYS,
                     ],
                 ],
             ],
-            'stations' => [
+            'stations'    => [
                 'label' => __('Stations'),
-                'icon' => 'volume_up',
+                'icon'  => 'volume_up',
                 'items' => [
                     'manage_stations' => [
-                        'label' => __('Stations'),
-                        'url' => (string)$router->named('admin:stations:index'),
+                        'label'      => __('Stations'),
+                        'url'        => (string)$router->named('admin:stations:index'),
                         'permission' => Acl::GLOBAL_STATIONS,
                     ],
-                    'custom_fields' => [
-                        'label' => __('Custom Fields'),
-                        'url' => (string)$router->named('admin:custom_fields:index'),
+                    'custom_fields'   => [
+                        'label'      => __('Custom Fields'),
+                        'url'        => (string)$router->named('admin:custom_fields:index'),
                         'permission' => Acl::GLOBAL_CUSTOM_FIELDS,
                     ],
-                    'relays' => [
-                        'label' => __('Connected AzuraRelays'),
-                        'url' => (string)$router->named('admin:relays:index'),
+                    'relays'          => [
+                        'label'      => __('Connected AzuraRelays'),
+                        'url'        => (string)$router->named('admin:relays:index'),
                         'permission' => Acl::GLOBAL_STATIONS,
                     ],
-                    'shoutcast' => [
-                        'label' => __('Install SHOUTcast'),
-                        'url' => (string)$router->named('admin:install_shoutcast:index'),
+                    'shoutcast'       => [
+                        'label'      => __('Install SHOUTcast'),
+                        'url'        => (string)$router->named('admin:install_shoutcast:index'),
                         'permission' => Acl::GLOBAL_ALL,
                     ],
-                    'geolite' => [
-                        'label' => __('Install GeoLite IP Database'),
-                        'url' => (string)$router->named('admin:install_geolite:index'),
+                    'geolite'         => [
+                        'label'      => __('Install GeoLite IP Database'),
+                        'url'        => (string)$router->named('admin:install_geolite:index'),
                         'permission' => Acl::GLOBAL_ALL,
                     ],
                 ],
--- config/menus/station.php
+++ config/menus/station.php
@@ -17,14 +17,14 @@ return function (App\Event\BuildStationMenu $e) {
 
     $e->merge(
         [
-            'start_station' => [
-                'label' => __('Start Station'),
-                'title' => __('Ready to start broadcasting? Click to start your station.'),
-                'icon' => 'refresh',
-                'url' => (string)$router->fromHere('api:stations:restart'),
-                'class' => 'api-call text-success',
-                'confirm' => __('Restart broadcasting? This will disconnect any current listeners.'),
-                'visible' => !$station->getHasStarted(),
+            'start_station'   => [
+                'label'      => __('Start Station'),
+                'title'      => __('Ready to start broadcasting? Click to start your station.'),
+                'icon'       => 'refresh',
+                'url'        => (string)$router->fromHere('api:stations:restart'),
+                'class'      => 'api-call text-success',
+                'confirm'    => __('Restart broadcasting? This will disconnect any current listeners.'),
+                'visible'    => !$station->getHasStarted(),
                 'permission' => Acl::STATION_BROADCASTING,
             ],
             'restart_station' => [
@@ -38,160 +38,160 @@ return function (App\Event\BuildStationMenu $e) {
                 'visible'    => $station->getHasStarted(),
                 'permission' => Acl::STATION_BROADCASTING,
             ],
-            'profile' => [
+            'profile'         => [
                 'label' => __('Profile'),
-                'icon' => 'image',
-                'url' => (string)$router->fromHere('stations:profile:index'),
+                'icon'  => 'image',
+                'url'   => (string)$router->fromHere('stations:profile:index'),
             ],
-            'public' => [
-                'label' => __('Public Page'),
-                'icon' => 'public',
-                'url' => (string)$router->named('public:index', ['station_id' => $station->getShortName()]),
+            'public'          => [
+                'label'    => __('Public Page'),
+                'icon'     => 'public',
+                'url'      => (string)$router->named('public:index', ['station_id' => $station->getShortName()]),
                 'external' => true,
-                'visible' => $station->getEnablePublicPage(),
+                'visible'  => $station->getEnablePublicPage(),
             ],
-            'ondemand' => [
-                'label' => __('On-Demand Media'),
-                'icon' => 'cloud_download',
-                'url' => (string)$router->named('public:ondemand', ['station_id' => $station->getShortName()]),
+            'ondemand'        => [
+                'label'    => __('On-Demand Media'),
+                'icon'     => 'cloud_download',
+                'url'      => (string)$router->named('public:ondemand', ['station_id' => $station->getShortName()]),
                 'external' => true,
-                'visible' => $station->getEnableOnDemand(),
+                'visible'  => $station->getEnableOnDemand(),
             ],
-            'files' => [
-                'label' => __('Music Files'),
-                'icon' => 'library_music',
-                'url' => (string)$router->fromHere('stations:files:index'),
-                'visible' => $backend->supportsMedia(),
+            'files'           => [
+                'label'      => __('Music Files'),
+                'icon'       => 'library_music',
+                'url'        => (string)$router->fromHere('stations:files:index'),
+                'visible'    => $backend->supportsMedia(),
                 'permission' => Acl::STATION_MEDIA,
             ],
-            'playlists' => [
-                'label' => __('Playlists'),
-                'icon' => 'queue_music',
-                'url' => (string)$router->fromHere('stations:playlists:index'),
-                'visible' => $backend->supportsMedia(),
+            'playlists'       => [
+                'label'      => __('Playlists'),
+                'icon'       => 'queue_music',
+                'url'        => (string)$router->fromHere('stations:playlists:index'),
+                'visible'    => $backend->supportsMedia(),
                 'permission' => Acl::STATION_MEDIA,
             ],
-            'podcasts' => [
-                'label' => __('Podcasts (Beta)'),
-                'icon' => 'cast',
-                'url' => (string)$router->fromHere('stations:podcasts:index'),
+            'podcasts'        => [
+                'label'      => __('Podcasts'),
+                'icon'       => 'cast',
+                'url'        => (string)$router->fromHere('stations:podcasts:index'),
                 'permission' => Acl::STATION_PODCASTS,
             ],
-            'streamers' => [
-                'label' => __('Streamer/DJ Accounts'),
-                'icon' => 'mic',
-                'url' => (string)$router->fromHere('stations:streamers:index'),
-                'visible' => $backend->supportsStreamers(),
+            'streamers'       => [
+                'label'      => __('Streamer/DJ Accounts'),
+                'icon'       => 'mic',
+                'url'        => (string)$router->fromHere('stations:streamers:index'),
+                'visible'    => $backend->supportsStreamers(),
                 'permission' => Acl::STATION_STREAMERS,
             ],
-            'web_dj' => [
-                'label' => __('Web DJ'),
-                'icon' => 'surround_sound',
-                'url' => (string)$router->named('public:dj', ['station_id' => $station->getShortName()], [], true)
+            'web_dj'          => [
+                'label'    => __('Web DJ'),
+                'icon'     => 'surround_sound',
+                'url'      => (string)$router->named('public:dj', ['station_id' => $station->getShortName()], [], true)
                     ->withScheme('https'),
-                'visible' => $station->getEnablePublicPage() && $station->getEnableStreamers(),
+                'visible'  => $station->getEnablePublicPage() && $station->getEnableStreamers(),
                 'external' => true,
             ],
-            'mounts' => [
-                'label' => __('Mount Points'),
-                'icon' => 'wifi_tethering',
-                'url' => (string)$router->fromHere('stations:mounts:index'),
-                'visible' => $frontend->supportsMounts(),
+            'mounts'          => [
+                'label'      => __('Mount Points'),
+                'icon'       => 'wifi_tethering',
+                'url'        => (string)$router->fromHere('stations:mounts:index'),
+                'visible'    => $frontend->supportsMounts(),
                 'permission' => Acl::STATION_MOUNTS,
             ],
-            'remotes' => [
-                'label' => __('Remote Relays'),
-                'icon' => 'router',
-                'url' => (string)$router->fromHere('stations:remotes:index'),
+            'remotes'         => [
+                'label'      => __('Remote Relays'),
+                'icon'       => 'router',
+                'url'        => (string)$router->fromHere('stations:remotes:index'),
                 'permission' => Acl::STATION_REMOTES,
             ],
-            'webhooks' => [
-                'label' => __('Web Hooks'),
-                'icon' => 'code',
-                'url' => (string)$router->fromHere('stations:webhooks:index'),
+            'webhooks'        => [
+                'label'      => __('Web Hooks'),
+                'icon'       => 'code',
+                'url'        => (string)$router->fromHere('stations:webhooks:index'),
                 'permission' => Acl::STATION_WEB_HOOKS,
             ],
-            'reports' => [
-                'label' => __('Reports'),
-                'icon' => 'assignment',
+            'reports'         => [
+                'label'      => __('Reports'),
+                'icon'       => 'assignment',
                 'permission' => Acl::STATION_REPORTS,
-                'items' => [
-                    'reports_overview' => [
+                'items'      => [
+                    'reports_overview'      => [
                         'label' => __('Statistics Overview'),
-                        'url' => (string)$router->fromHere('stations:reports:overview'),
+                        'url'   => (string)$router->fromHere('stations:reports:overview'),
                     ],
-                    'reports_listeners' => [
+                    'reports_listeners'     => [
                         'label' => __('Listeners'),
-                        'url' => (string)$router->fromHere('stations:reports:listeners'),
+                        'url'   => (string)$router->fromHere('stations:reports:listeners'),
                     ],
-                    'reports_requests' => [
-                        'label' => __('Song Requests'),
-                        'url' => (string)$router->fromHere('stations:reports:requests'),
+                    'reports_requests'      => [
+                        'label'   => __('Song Requests'),
+                        'url'     => (string)$router->fromHere('stations:reports:requests'),
                         'visible' => $station->getEnableRequests(),
                     ],
-                    'reports_timeline' => [
+                    'reports_timeline'      => [
                         'label' => __('Song Playback Timeline'),
-                        'url' => (string)$router->fromHere('stations:reports:timeline'),
+                        'url'   => (string)$router->fromHere('stations:reports:timeline'),
                     ],
-                    'reports_performance' => [
-                        'label' => __('Song Listener Impact'),
-                        'url' => (string)$router->fromHere('stations:reports:performance'),
+                    'reports_performance'   => [
+                        'label'   => __('Song Listener Impact'),
+                        'url'     => (string)$router->fromHere('stations:reports:performance'),
                         'visible' => $backend->supportsMedia(),
                     ],
-                    'reports_duplicates' => [
-                        'label' => __('Duplicate Songs'),
-                        'url' => (string)$router->fromHere('stations:files:index') . '#special:duplicates',
+                    'reports_duplicates'    => [
+                        'label'   => __('Duplicate Songs'),
+                        'url'     => (string)$router->fromHere('stations:files:index') . '#special:duplicates',
                         'visible' => $backend->supportsMedia(),
                     ],
                     'reports_unprocessable' => [
-                        'label' => __('Unprocessable Files'),
-                        'url' => (string)$router->fromHere('stations:files:index') . '#special:unprocessable',
+                        'label'   => __('Unprocessable Files'),
+                        'url'     => (string)$router->fromHere('stations:files:index') . '#special:unprocessable',
                         'visible' => $backend->supportsMedia(),
                     ],
                     'reports_soundexchange' => [
                         'label' => __('SoundExchange Royalties'),
-                        'url' => (string)$router->fromHere('stations:reports:soundexchange'),
+                        'url'   => (string)$router->fromHere('stations:reports:soundexchange'),
                     ],
                 ],
             ],
-            'utilities' => [
+            'utilities'       => [
                 'label' => __('Utilities'),
-                'icon' => 'settings',
+                'icon'  => 'settings',
                 'items' => [
                     'sftp_users' => [
-                        'label' => __('SFTP Users'),
-                        'url' => (string)$router->fromHere('stations:sftp_users:index'),
-                        'visible' => App\Service\SftpGo::isSupportedForStation($station),
+                        'label'      => __('SFTP Users'),
+                        'url'        => (string)$router->fromHere('stations:sftp_users:index'),
+                        'visible'    => App\Service\SftpGo::isSupportedForStation($station),
                         'permission' => Acl::STATION_MEDIA,
                     ],
                     'automation' => [
-                        'label' => __('Automated Assignment'),
-                        'url' => (string)$router->fromHere('stations:automation:index'),
-                        'visible' => $backend->supportsMedia(),
+                        'label'      => __('Automated Assignment'),
+                        'url'        => (string)$router->fromHere('stations:automation:index'),
+                        'visible'    => $backend->supportsMedia(),
                         'permission' => Acl::STATION_AUTOMATION,
                     ],
-                    'ls_config' => [
-                        'label' => __('Edit Liquidsoap Configuration'),
-                        'url' => (string)$router->fromHere('stations:util:ls_config'),
-                        'visible' => $settings->getEnableAdvancedFeatures()
+                    'ls_config'  => [
+                        'label'      => __('Edit Liquidsoap Configuration'),
+                        'url'        => (string)$router->fromHere('stations:util:ls_config'),
+                        'visible'    => $settings->getEnableAdvancedFeatures()
                             && $backend instanceof App\Radio\Backend\Liquidsoap,
                         'permission' => Acl::STATION_BROADCASTING,
                     ],
-                    'logs' => [
-                        'label' => __('Log Viewer'),
-                        'url' => (string)$router->fromHere('stations:logs:index'),
+                    'logs'       => [
+                        'label'      => __('Log Viewer'),
+                        'url'        => (string)$router->fromHere('stations:logs:index'),
                         'permission' => Acl::STATION_LOGS,
                     ],
-                    'queue' => [
-                        'label' => __('Upcoming Song Queue'),
-                        'url' => (string)$router->fromHere('stations:queue:index'),
+                    'queue'      => [
+                        'label'      => __('Upcoming Song Queue'),
+                        'url'        => (string)$router->fromHere('stations:queue:index'),
                         'permission' => Acl::STATION_BROADCASTING,
                     ],
-                    'restart' => [
-                        'label' => __('Restart Broadcasting'),
-                        'url' => (string)$router->fromHere('api:stations:restart'),
-                        'class' => 'api-call',
-                        'confirm' => __('Restart broadcasting? This will disconnect any current listeners.'),
+                    'restart'    => [
+                        'label'      => __('Restart Broadcasting'),
+                        'url'        => (string)$router->fromHere('api:stations:restart'),
+                        'class'      => 'api-call',
+                        'confirm'    => __('Restart broadcasting? This will disconnect any current listeners.'),
                         'permission' => Acl::STATION_BROADCASTING,
                     ],
                 ],

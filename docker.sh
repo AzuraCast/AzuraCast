21b18d46891b7b1d1e1a30059a8770b057595733
Mitch <crossyy@pm.me>
2022-01-07 08:59:36 +0000

    Give more details to users when using the letsencrypt create command.  (#4956)
    * Update docker.sh
    
    * Update docker.sh
    
    Co-authored-by: Buster "Silver Eagle" Neece <buster@busterneece.com>

--- docker.sh
+++ docker.sh
@@ -772,8 +772,12 @@ uninstall() {
 # Usage: ./docker.sh letsencrypt-create
 #
 letsencrypt-create() {
-  setup-letsencrypt
+  echo "This script will automatically attempt to update your .env file, but if you see no response, you can:"
+  echo "  - Manually edit .env and provide a LETSENCRYPT_HOST and LETSENCRYPT_EMAIL"
+  echo "  - Stop existing services by running docker-compose down"
+  echo "  - Start services again by running docker-compose up -d"
 
+  setup-letsencrypt
   docker-compose down
   docker-compose up -d
   exit

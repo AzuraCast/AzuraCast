135f410cdd979f6541e8a155f55479081cc38921
Buster "Silver Eagle" Neece <buster@busterneece.com>
2022-01-17 06:01:44 -0600

    WIP Nginx consolidation.

---
+++ util/docker/web/startup_scripts/01_self_signed_ssl.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+# Generate a self-signed certificate if one doesn't exist in the certs path.
+if [ ! -f /etc/nginx/certs/default.crt ]; then 
+    echo "Generating self-signed certificate..."
+
+    openssl req -new -nodes -x509 -subj "/C=US/ST=Texas/L=Austin/O=IT/CN=localhost" \
+        -days 365 -extensions v3_ca \
+        -keyout /etc/nginx/certs/default.key \
+        -out /etc/nginx/certs/default.crt
+fi

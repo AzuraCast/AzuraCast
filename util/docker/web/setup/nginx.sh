8e57d2e04b2cec23721512c65c720a53fc30cf2b
Buster "Silver Eagle" Neece <buster@busterneece.com>
2022-01-24 07:11:34 -0600

    nginx cleanup.

--- util/docker/web/setup/nginx.sh
+++ util/docker/web/setup/nginx.sh
@@ -6,13 +6,14 @@ set -x
 $minimal_apt_get_install nginx nginx-common nginx-extras openssl
 
 # Install nginx and configuration
-cp /bd_build/nginx/nginx.conf /etc/nginx/nginx.conf
+cp /bd_build/nginx/proxy_params.conf /etc/nginx/proxy_params
+cp /bd_build/nginx/nginx.conf.tmpl /etc/nginx/nginx.conf.tmpl
 cp /bd_build/nginx/azuracast.conf.tmpl /etc/nginx/azuracast.conf.tmpl
 
 mkdir -p /etc/nginx/azuracast.conf.d/
 
 # Create nginx temp dirs
-mkdir -p /tmp/azuracast_nginx_client /tmp/azuracast_fastcgi_temp
-touch /tmp/azuracast_nginx_client/.tmpreaper
-touch /tmp/azuracast_fastcgi_temp/.tmpreaper
-chmod -R 777 /tmp/azuracast_*
+mkdir -p /tmp/app_nginx_client /tmp/app_fastcgi_temp
+touch /tmp/app_nginx_client/.tmpreaper
+touch /tmp/app_fastcgi_temp/.tmpreaper
+chmod -R 777 /tmp/app_*

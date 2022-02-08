7a348a7440711270f51b341b43856c1575a54c98
Vaalyn <vaalyndev@gmail.com>
2022-02-08 23:20:35 +0100

    Add FFMpeg binary to Ansible base dependencies

--- update.sh
+++ update.sh
@@ -34,7 +34,7 @@ else
 fi
 
 APP_ENV="${APP_ENV:-production}"
-UPDATE_REVISION="${UPDATE_REVISION:-72}"
+UPDATE_REVISION="${UPDATE_REVISION:-73}"
 
 echo "Updating AzuraCast (Environment: $APP_ENV, Update revision: $UPDATE_REVISION)"
 

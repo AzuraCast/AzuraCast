e61109d5b5f4808729111b6d32297aea154f2a58
Buster "Silver Eagle" Neece <buster@busterneece.com>
2022-03-05 19:13:24 -0600

    Fine-tune mariadb and redis.

--- util/docker/mariadb/setup/mariadb.sh
+++ util/docker/mariadb/setup/mariadb.sh
@@ -3,6 +3,8 @@ set -e
 source /bd_build/buildconfig
 set -x
 
+$minimal_apt_get_install tzdata
+
 echo "1" >> /etc/container_environment/MARIADB_AUTO_UPGRADE
 
 cp /bd_build/mariadb/mariadb/db.sql /docker-entrypoint-initdb.d/00-azuracast.sql

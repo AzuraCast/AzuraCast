0d54e9e1958b491d55e52b12386508978e2a7ab5
Buster "Silver Eagle" Neece <buster@busterneece.com>
2022-04-19 05:19:54 -0500

    Remove "Playlist: " and "Schedule: " prefix on global schedule view.

--- src/Entity/ApiGenerator/ScheduleApiGenerator.php
+++ src/Entity/ApiGenerator/ScheduleApiGenerator.php
@@ -52,11 +52,11 @@ class ScheduleApiGenerator
         if ($playlist instanceof Entity\StationPlaylist) {
             $row->type = Entity\Api\StationSchedule::TYPE_PLAYLIST;
             $row->name = $playlist->getName();
-            $row->title = __('Playlist: %s', $row->name);
+            $row->title = $row->name;
         } elseif ($streamer instanceof Entity\StationStreamer) {
             $row->type = Entity\Api\StationSchedule::TYPE_STREAMER;
             $row->name = $streamer->getDisplayName();
-            $row->title = __('Streamer: %s', $row->name);
+            $row->title = $row->name;
         }
 
         return $row;

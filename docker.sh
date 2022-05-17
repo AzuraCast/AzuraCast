3d8ada872c599c79ac1a47b11a72ffbdb84221ba
Vaalyn <vaalyndev@gmail.com>
2022-05-17 19:27:30 +0200

    Fix frontend build process for dev and missing admin/station menus (#5382)
    * Fix path for frontend docker-compose.yml in static() command
    
    * Set correct HOME env var for node user
    
    * Revert removal of isset in AbstractBuildMenu event

--- docker.sh
+++ docker.sh
@@ -763,9 +763,9 @@ restore-legacy() {
 # Usage: ./docker.sh static [static_container_command]
 #
 static() {
-  docker-compose -f frontend/docker-compose.yml down -v
-  docker-compose -f frontend/docker-compose.yml build
-  docker-compose --env-file=.env -f frontend/docker-compose.yml run --rm frontend "$@"
+  docker-compose -f docker-compose.frontend.yml down -v
+  docker-compose -f docker-compose.frontend.yml build
+  docker-compose --env-file=.env -f docker-compose.frontend.yml run --rm frontend "$@"
   exit
 }
 

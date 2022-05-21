76f5b057c69ee48447a228901520257065b083f6
Buster "Silver Eagle" Neece <buster@busterneece.com>
2022-05-21 16:19:30 -0500

    Remove Advanced playlist exception on LS ConfigWriter.

--- src/Radio/Backend/Liquidsoap/ConfigWriter.php
+++ src/Radio/Backend/Liquidsoap/ConfigWriter.php
@@ -330,8 +330,8 @@ class ConfigWriter implements EventSubscriberInterface
                             $buffer = ($buffer < 1) ? Entity\StationPlaylist::DEFAULT_REMOTE_BUFFER : $buffer;
 
                             $playlistConfigLines[] = $playlistVarName . ' = mksafe(buffer(buffer=' . $buffer . '., input.http(max_buffer=' . $buffer . '., "' . self::cleanUpString(
-                                $remote_url
-                            ) . '")))';
+                                    $remote_url
+                                ) . '")))';
                         }
                         break;
                 }
@@ -424,8 +424,6 @@ class ConfigWriter implements EventSubscriberInterface
                         }
                     }
                     break;
-                case Entity\Enums\PlaylistTypes::Advanced:
-                    throw new \Exception('To be implemented');
             }
         }
 

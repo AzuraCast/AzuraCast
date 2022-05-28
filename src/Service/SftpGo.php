719ded0ff2514bd46d7343be84124803a4e89999
Buster "Silver Eagle" Neece <buster@busterneece.com>
2022-05-27 23:32:54 -0500

    Add sftpgo to Ansible.

--- src/Service/SftpGo.php
+++ src/Service/SftpGo.php
@@ -5,19 +5,11 @@ declare(strict_types=1);
 namespace App\Service;
 
 use App\Entity\Station;
-use App\Environment;
 
 class SftpGo
 {
-    public static function isSupported(): bool
-    {
-        $environment = Environment::getInstance();
-        return !$environment->isTesting() && $environment->isDocker();
-    }
-
     public static function isSupportedForStation(Station $station): bool
     {
-        $mediaStorage = $station->getMediaStorageLocation();
-        return $mediaStorage->isLocal() && self::isSupported();
+        return $station->getMediaStorageLocation()->isLocal();
     }
 }

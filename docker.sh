124b5dc8a962032c2c5c38fec560828f1161edbe
Buster Neece <buster@busterneece.com>
2022-09-06 19:52:15 -0500

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -798,8 +798,8 @@ uninstall() {
 #
 setup-letsencrypt() {
   echo "LetsEncrypt is now managed from within the web interface."
-  echo "You can manage it via the Administration button then System Settings."
-  echo "Under Services you can find the LetsEncrypt panel, you can modify the settings then press save" 
+  echo "You can manage it via the 'Administration' panel, then 'System Settings'."
+  echo "Under 'Services' you will find the LetsEncrypt settings." 
 }
 
 letsencrypt-create() {

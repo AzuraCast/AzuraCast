fbf4df9ae2b58bf4fcc17ed27c6d6f1f65d9944d
Buster Neece <buster@busterneece.com>
2022-12-11 17:46:50 -0600

    Fix gettext.

--- frontend/vue/vendor/gettext.js
+++ frontend/vue/vendor/gettext.js
@@ -2,7 +2,7 @@ import translations from "../../../translations/translations.json";
 import {createGettext} from "vue3-gettext";
 
 export default createGettext({
-    defaultLanguage: 'en_US',
+    defaultLanguage: App.locale,
     translations: translations,
     silent: true
 });

8248f1f926343767c3dc2f2a6e52ac0b8065492a
Buster Neece <buster@busterneece.com>
2022-12-29 15:05:04 -0600

    Revert "Fixes #5853 -- Fix Liquidsoap operator for "play single track" advanced state."
    This reverts commit b0549b4700371ce77179c834e63c101a2cbe0142.

--- src/Radio/Backend/Liquidsoap/ConfigWriter.php
+++ src/Radio/Backend/Liquidsoap/ConfigWriter.php
@@ -365,7 +365,7 @@ final class ConfigWriter implements EventSubscriberInterface
             $event->appendLines($playlistConfigLines);
 
             if ($playlist->backendPlaySingleTrack()) {
-                $playlistVarName = 'predicate.once(' . $playlistVarName . ')';
+                $playlistVarName = 'once(' . $playlistVarName . ')';
             }
 
             switch ($playlist->getTypeEnum()) {

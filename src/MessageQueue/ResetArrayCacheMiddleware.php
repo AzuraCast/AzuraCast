40b6aadec07818d83785455c5581fb56a6251dbc
Buster Neece <buster@busterneece.com>
2023-02-20 06:29:17 -0600

    Make array cache resetter ArrayCache-specific.

--- src/MessageQueue/ResetArrayCacheMiddleware.php
+++ src/MessageQueue/ResetArrayCacheMiddleware.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace App\MessageQueue;
 
-use Symfony\Component\Cache\ResettableInterface;
+use Symfony\Component\Cache\Adapter\ArrayAdapter;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\Messenger\Event\WorkerMessageReceivedEvent;
 use Symfony\Contracts\Cache\CacheInterface;
@@ -30,7 +30,7 @@ final class ResetArrayCacheMiddleware implements EventSubscriberInterface
 
     public function resetArrayCache(WorkerMessageReceivedEvent $event): void
     {
-        if ($this->cache instanceof ResettableInterface) {
+        if ($this->cache instanceof ArrayAdapter) {
             $this->cache->reset();
         }
     }

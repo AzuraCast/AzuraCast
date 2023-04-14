d62dd5599b60122a7b6ab4d2152d98efd128945d
Buster Neece <buster@busterneece.com>
2023-04-14 14:22:11 -0500

    Move master_me from a build to an install step.

---
+++ util/docker/stations/setup/master_me.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+set -x
+
+mkdir -p /tmp/master_me
+cd /tmp/master_me
+
+# Per-architecture LS installs
+ARCHITECTURE=x86_64
+if [[ "$(uname -m)" = "aarch64" ]]; then
+    ARCHITECTURE=arm64
+fi
+
+wget -O master_me.tar.xz "https://github.com/trummerschlunk/master_me/releases/download/1.1.0/master_me-1.1.0-linux-${ARCHITECTURE}.tar.xz"
+
+tar -xvf master_me.tar.xz --strip-components=1
+
+mkdir -p /usr/lib/lv2
+mv ./master_me-easy-presets.lv2 /usr/lib/lv2
+mv ./master_me.lv2 /usr/lib/lv2

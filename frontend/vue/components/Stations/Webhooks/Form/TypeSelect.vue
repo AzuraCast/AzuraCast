3d51cf3d8f706a122d65802372a7f376e5e7e84e
Buster Neece <buster@busterneece.com>
2023-05-23 19:58:52 -0500

    Fix styling on list groups and webhook type selection.

--- frontend/vue/components/Stations/Webhooks/Form/TypeSelect.vue
+++ frontend/vue/components/Stations/Webhooks/Form/TypeSelect.vue
@@ -6,72 +6,95 @@
 
         <div class="form-row">
             <div class="col-md-6">
-                <h6>{{ $gettext('Generic Web Hooks') }}</h6>
-                <b-list-group class="mb-3">
-                    <type-select-row
-                        :type="WEBHOOK_TYPE_GENERIC"
-                        :types="typeDetails"
-                        @select="selectType"
-                    />
-                    <type-select-row
-                        :type="WEBHOOK_TYPE_EMAIL"
-                        :types="typeDetails"
-                        @select="selectType"
-                    />
-                </b-list-group>
-
-                <h6>{{ $gettext('Social Media') }}</h6>
-                <b-list-group class="mb-3">
-                    <type-select-row
-                        :type="WEBHOOK_TYPE_DISCORD"
-                        :types="typeDetails"
-                        @select="selectType"
-                    />
-                    <type-select-row
-                        :type="WEBHOOK_TYPE_TELEGRAM"
-                        :types="typeDetails"
-                        @select="selectType"
-                    />
-                    <type-select-row
-                        :type="WEBHOOK_TYPE_MASTODON"
-                        :types="typeDetails"
-                        @select="selectType"
-                    />
-                    <type-select-row
-                        :type="WEBHOOK_TYPE_TWITTER"
-                        :types="typeDetails"
-                        @select="selectType"
-                    />
-                </b-list-group>
+                <section class="card mb-3">
+                    <div class="card-header bg-primary-dark">
+                        <h2 class="card-title">
+                            {{ $gettext('Generic Web Hooks') }}
+                        </h2>
+                    </div>
+                    <b-list-group>
+                        <type-select-row
+                            :type="WEBHOOK_TYPE_GENERIC"
+                            :types="typeDetails"
+                            @select="selectType"
+                        />
+                        <type-select-row
+                            :type="WEBHOOK_TYPE_EMAIL"
+                            :types="typeDetails"
+                            @select="selectType"
+                        />
+                    </b-list-group>
+                </section>
+                <section class="card mb-3">
+                    <div class="card-header bg-primary-dark">
+                        <h2 class="card-title">
+                            {{ $gettext('Social Media') }}
+                        </h2>
+                    </div>
+                    <b-list-group>
+                        <type-select-row
+                            :type="WEBHOOK_TYPE_DISCORD"
+                            :types="typeDetails"
+                            @select="selectType"
+                        />
+                        <type-select-row
+                            :type="WEBHOOK_TYPE_TELEGRAM"
+                            :types="typeDetails"
+                            @select="selectType"
+                        />
+                        <type-select-row
+                            :type="WEBHOOK_TYPE_MASTODON"
+                            :types="typeDetails"
+                            @select="selectType"
+                        />
+                        <type-select-row
+                            :type="WEBHOOK_TYPE_TWITTER"
+                            :types="typeDetails"
+                            @select="selectType"
+                        />
+                    </b-list-group>
+                </section>
             </div>
             <div class="col-md-6">
-                <h6>{{ $gettext('Station Directories') }}</h6>
-                <b-list-group class="mb-3">
-                    <type-select-row
-                        :type="WEBHOOK_TYPE_TUNEIN"
-                        :types="typeDetails"
-                        @select="selectType"
-                    />
-                </b-list-group>
+                <section class="card mb-3">
+                    <div class="card-header bg-primary-dark">
+                        <h2 class="card-title">
+                            {{ $gettext('Station Directories') }}
+                        </h2>
+                    </div>
+                    <b-list-group>
+                        <type-select-row
+                            :type="WEBHOOK_TYPE_TUNEIN"
+                            :types="typeDetails"
+                            @select="selectType"
+                        />
+                    </b-list-group>
+                </section>
 
-                <h6>{{ $gettext('Analytics') }}</h6>
-                <b-list-group class="mb-3">
-                    <type-select-row
-                        :type="WEBHOOK_TYPE_GOOGLE_ANALYTICS_V3"
-                        :types="typeDetails"
-                        @select="selectType"
-                    />
-                    <type-select-row
-                        :type="WEBHOOK_TYPE_GOOGLE_ANALYTICS_V4"
-                        :types="typeDetails"
-                        @select="selectType"
-                    />
-                    <type-select-row
-                        :type="WEBHOOK_TYPE_MATOMO_ANALYTICS"
-                        :types="typeDetails"
-                        @select="selectType"
-                    />
-                </b-list-group>
+                <section class="card mb-3">
+                    <div class="card-header bg-primary-dark">
+                        <h2 class="card-title">
+                            {{ $gettext('Analytics') }}
+                        </h2>
+                    </div>
+                    <b-list-group>
+                        <type-select-row
+                            :type="WEBHOOK_TYPE_GOOGLE_ANALYTICS_V3"
+                            :types="typeDetails"
+                            @select="selectType"
+                        />
+                        <type-select-row
+                            :type="WEBHOOK_TYPE_GOOGLE_ANALYTICS_V4"
+                            :types="typeDetails"
+                            @select="selectType"
+                        />
+                        <type-select-row
+                            :type="WEBHOOK_TYPE_MATOMO_ANALYTICS"
+                            :types="typeDetails"
+                            @select="selectType"
+                        />
+                    </b-list-group>
+                </section>
             </div>
         </div>
     </b-form-group>

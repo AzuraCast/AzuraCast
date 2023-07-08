261be5e2a85c0e12f82acad772cc2f6c5df890d9
Buster Neece <buster@busterneece.com>
2023-07-08 17:25:29 -0500

    Make copy-to-clipboard button have feedback.

--- frontend/vue/components/Common/CopyToClipboardButton.vue
+++ frontend/vue/components/Common/CopyToClipboardButton.vue
@@ -9,13 +9,14 @@
             class="sm"
             icon="file_copy"
         />
-        <span v-if="!hideText">{{ $gettext('Copy to Clipboard') }}</span>
+        <span v-if="!hideText">{{ copyText }}</span>
     </button>
 </template>
 
 <script setup>
 import Icon from "~/components/Common/Icon.vue";
-import {useClipboard} from "@vueuse/core";
+import {refAutoReset, useClipboard} from "@vueuse/core";
+import {useTranslate} from "~/vendor/gettext";
 
 const props = defineProps({
     text: {
@@ -28,9 +29,17 @@ const props = defineProps({
     }
 });
 
+const {$gettext} = useTranslate();
+
+const copyText = refAutoReset(
+    $gettext('Copy to Clipboard'),
+    1000
+);
+
 const clipboard = useClipboard({legacy: true});
 
 const doCopy = () => {
     clipboard.copy(props.text);
+    copyText.value = $gettext('Copied!');
 };
 </script>

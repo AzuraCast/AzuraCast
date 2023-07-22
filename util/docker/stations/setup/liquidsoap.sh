6a1780445318c281922507b4dceae3298179bb69
Buster Neece <buster@busterneece.com>
2023-07-22 17:17:27 -0500

    Update to Liquidsoap release version 2.2.0.

--- util/docker/stations/setup/liquidsoap.sh
+++ util/docker/stations/setup/liquidsoap.sh
@@ -19,8 +19,8 @@ if [[ "$(uname -m)" = "aarch64" ]]; then
     ARCHITECTURE=arm64
 fi
 
-# wget -O /tmp/liquidsoap.deb "https://github.com/savonet/liquidsoap/releases/download/v2.1.4/liquidsoap_2.1.4-ubuntu-jammy-1_${ARCHITECTURE}.deb"
-wget -O /tmp/liquidsoap.deb "https://github.com/savonet/liquidsoap/releases/download/rolling-release-v2.2.x/liquidsoap-4f236df_2.2.0-ubuntu-jammy-1_${ARCHITECTURE}.deb"
+wget -O /tmp/liquidsoap.deb "https://github.com/savonet/liquidsoap/releases/download/v2.2.0/liquidsoap_2.2.0-ubuntu-jammy-1_${ARCHITECTURE}.deb"
+# wget -O /tmp/liquidsoap.deb "https://github.com/savonet/liquidsoap/releases/download/rolling-release-v2.2.x/liquidsoap-4f236df_2.2.0-ubuntu-jammy-1_${ARCHITECTURE}.deb"
 
 dpkg -i /tmp/liquidsoap.deb
 apt-get install -y -f --no-install-recommends

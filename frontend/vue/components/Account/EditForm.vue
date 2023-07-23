98ce9b51c498f9443d3e7c5a81c905bc3916c99a
Buster Neece <buster@busterneece.com>
2023-07-22 21:27:18 -0500

    Make account edit form more vertically compact.

--- frontend/vue/components/Account/EditForm.vue
+++ frontend/vue/components/Account/EditForm.vue
@@ -1,41 +1,20 @@
 <template>
-    <form-fieldset>
-        <div class="row g-3">
+    <div class="row g-3">
+        <div class="col-md-6">
             <form-group-field
                 id="form_name"
-                class="col-md-6"
                 :field="form.name"
                 :label="$gettext('Name')"
             />
 
             <form-group-field
                 id="form_email"
-                class="col-md-6"
                 :field="form.email"
                 :label="$gettext('E-mail Address')"
-            />
-        </div>
-    </form-fieldset>
-
-    <form-fieldset>
-        <template #label>
-            {{ $gettext('Customization') }}
-        </template>
-
-        <div class="row g-3">
-            <form-group-multi-check
-                id="edit_form_locale"
-                class="col-md-6"
-                :field="form.locale"
-                :options="localeOptions"
-                stacked
-                radio
-                :label="$gettext('Language')"
             />
 
             <form-group-multi-check
                 id="edit_form_show_24_hour_time"
-                class="col-md-6"
                 :field="form.show_24_hour_time"
                 :options="show24hourOptions"
                 stacked
@@ -43,12 +22,21 @@
                 :label="$gettext('Time Display')"
             />
         </div>
-    </form-fieldset>
+        <div class="col-md-6">
+            <form-group-multi-check
+                id="edit_form_locale"
+                :field="form.locale"
+                :options="localeOptions"
+                stacked
+                radio
+                :label="$gettext('Language')"
+            />
+        </div>
+    </div>
 </template>
 
 <script setup>
 import FormGroupField from "~/components/Form/FormGroupField";
-import FormFieldset from "~/components/Form/FormFieldset";
 import objectToFormOptions from "~/functions/objectToFormOptions";
 import {computed} from "vue";
 import {useTranslate} from "~/vendor/gettext";

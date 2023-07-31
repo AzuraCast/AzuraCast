1404779b1a72ccbc0bd165ce0c4d8805b6024586
Buster Neece <buster@busterneece.com>
2023-07-30 21:11:51 -0500

    Properly check permissions on stats endpoint.

--- config/routes/api_admin.php
+++ config/routes/api_admin.php
@@ -58,7 +58,8 @@ return static function (RouteCollectorProxy $group) {
             )->add(new Middleware\Permissions(GlobalPermissions::Backups));
 
             $group->get('/server/stats', Controller\Api\Admin\ServerStatsAction::class)
-                ->setName('api:admin:server:stats');
+                ->setName('api:admin:server:stats')
+                ->add(new Middleware\Permissions(GlobalPermissions::View));
 
             $group->get(
                 '/services',

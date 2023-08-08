b5024067668ce39d6ada3f4021c7986172cd4b11
Buster Neece <buster@busterneece.com>
2023-08-08 08:35:47 -0500

    Implement frontend ACL and user object.

--- frontend/vue/vendor/azuracast.ts
+++ frontend/vue/vendor/azuracast.ts
@@ -1,6 +1,7 @@
 /* eslint-disable no-undef */
 
 import {App, inject, InjectionKey} from "vue";
+import {GlobalPermission, StationPermission} from "~/acl.ts";
 
 const globalPropsKey: InjectionKey<AzuraCastConstants> = Symbol() as InjectionKey<AzuraCastConstants>;
 
@@ -12,7 +13,16 @@ export interface AzuraCastStationConstants {
     id: number | null,
     name: string | null,
     shortName: string | null,
-    timezone: string
+    timezone: string | null
+}
+
+export interface AzuraCastUserConstants {
+    id: number | null,
+    displayName: string | null,
+    globalPermissions: GlobalPermission[],
+    stationPermissions: {
+        [key: number]: StationPermission[]
+    }
 }
 
 export interface AzuraCastConstants {
@@ -25,6 +35,7 @@ export interface AzuraCastConstants {
     panelProps: object | null,
     sidebarProps: object | null,
     componentProps: object | null,
+    user: AzuraCastUserConstants | null,
     station: AzuraCastStationConstants | null,
 }
 
@@ -32,7 +43,24 @@ export function useAzuraCast(): AzuraCastConstants {
     return inject(globalPropsKey);
 }
 
+export function useAzuraCastUser(): AzuraCastUserConstants {
+    const {user} = useAzuraCast();
+
+    return (user !== null) ? user : {
+        id: null,
+        displayName: null,
+        globalPermissions: [],
+        stationPermissions: {}
+    };
+}
+
 export function useAzuraCastStation(): AzuraCastStationConstants {
     const {station} = useAzuraCast();
-    return station;
+
+    return (station !== null) ? station : {
+        id: null,
+        name: null,
+        shortName: null,
+        timezone: null
+    };
 }

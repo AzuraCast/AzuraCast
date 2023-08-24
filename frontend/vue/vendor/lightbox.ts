298563e41c688f8db949c2eba19d0129409bca9b
Buster Neece <buster@busterneece.com>
2023-08-24 16:59:03 -0500

    Little TS stuff.

--- frontend/vue/vendor/lightbox.ts
+++ frontend/vue/vendor/lightbox.ts
@@ -1,14 +1,14 @@
 import Lightbox from "~/components/Common/Lightbox.vue";
 import {Directive, inject, InjectionKey, provide, Ref} from "vue";
 
-const provideKey: InjectionKey<Ref<Lightbox>> = Symbol() as InjectionKey<Ref<Lightbox>>;
+const provideKey: InjectionKey<Ref<typeof Lightbox>> = Symbol() as InjectionKey<Ref<typeof Lightbox>>;
 
-export function useProvideLightbox(lightboxRef: Ref<Lightbox>): void {
+export function useProvideLightbox(lightboxRef: Ref<typeof Lightbox>): void {
     provide(provideKey, lightboxRef);
 }
 
 export function useLightbox() {
-    const lightbox: Ref<Lightbox> = inject(provideKey);
+    const lightbox: Ref<typeof Lightbox> = inject(provideKey);
 
     const vLightbox: Directive<HTMLElement, string> = (el: HTMLElement): void => {
         el.addEventListener('click', (e: MouseEvent): void => {

07918312313476261b05de0276b9b51d1f197962
Buster Neece <buster@busterneece.com>
2023-10-01 00:33:10 -0500

    Update audiowaveform

--- util/docker/web/setup/00_packages.sh
+++ util/docker/web/setup/00_packages.sh
@@ -9,7 +9,7 @@ add-apt-repository -y ppa:ondrej/php
 apt-get update
 
 apt-get install -y --no-install-recommends \
-  audiowaveform=1.8.1-1jammy1 \
+  audiowaveform=1.9.0-1jammy1 \
   nginx nginx-common openssl \
   tmpreaper \
   zstd \

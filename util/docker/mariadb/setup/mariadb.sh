cc2fe2302940dcba79637ab7450af35f2ff8bac9
Buster Neece <buster@busterneece.com>
2023-12-12 11:16:13 -0600

    Revert mariadb changes.

--- util/docker/mariadb/setup/mariadb.sh
+++ util/docker/mariadb/setup/mariadb.sh
@@ -16,7 +16,8 @@ set -x
 apt-get update
 
 apt-get install -q -y --no-install-recommends \
-  mariadb-server mariadb-backup mariadb-client
+  mariadb-server mariadb-backup mariadb-client \
+  ca-certificates gpg gpgv libjemalloc2 pwgen tzdata xz-utils zstd
 
 rm -rf /var/lib/mysql
 mkdir -p /var/lib/mysql /var/run/mysqld

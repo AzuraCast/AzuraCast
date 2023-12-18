ece35f09affd647c7b168587157002e0e2c1702f
Buster Neece <buster@busterneece.com>
2023-12-17 19:50:01 -0600

    #6535 -- Add sftpgo temp files to the tmpreaper rotation.

--- util/docker/web/setup/sftpgo.sh
+++ util/docker/web/setup/sftpgo.sh
@@ -10,3 +10,8 @@ cp /bd_build/web/sftpgo/sftpgo.json /var/azuracast/sftpgo/sftpgo.json
 
 touch /var/azuracast/sftpgo/sftpgo.db
 chown -R azuracast:azuracast /var/azuracast/sftpgo
+
+# Create sftpgo temp dir
+mkdir -p /tmp/sftpgo_temp
+touch /tmp/sftpgo_temp/.tmpreaper
+chmod -R 777 /tmp/sftpgo_temp

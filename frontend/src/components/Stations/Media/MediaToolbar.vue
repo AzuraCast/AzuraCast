e9d349f96a20f242df3af46c8dd323428b394f29
Buster Neece <buster@busterneece.com>
2023-12-23 21:30:29 -0600

    Fixes #6814 -- Fix error when setting playlists on media.

--- frontend/src/components/Stations/Media/MediaToolbar.vue
+++ frontend/src/components/Stations/Media/MediaToolbar.vue
@@ -323,7 +323,9 @@ const setPlaylists = () => {
             'dirs': props.selectedItems.directories
         }).then(({data}) => {
             if (data.success) {
-                emit('add-playlist', data.record);
+                if (data.record) {
+                    emit('add-playlist', data.record);
+                }
 
                 checkedPlaylists.value = [];
                 newPlaylist.value = '';

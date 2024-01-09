3b8c7e9e00d5d8ab714955a0de8e8681ebb0a984
Buster Neece <buster@busterneece.com>
2024-01-09 04:24:43 -0600

    #6845 -- Fix routing on /api/station/(id)/nowplaying

--- config/routes/api_station.php
+++ config/routes/api_station.php
@@ -19,7 +19,7 @@ return static function (RouteCollectorProxy $group) {
                 ->setName('api:stations:index')
                 ->add(new Middleware\RateLimit('api', 5, 2));
 
-            $group->get('/nowplaying', Controller\Api\NowPlayingAction::class . ':getAction');
+            $group->get('/nowplaying', Controller\Api\NowPlayingAction::class);
 
             $group->get('/schedule', Controller\Api\Stations\ScheduleAction::class)
                 ->setName('api:stations:schedule');

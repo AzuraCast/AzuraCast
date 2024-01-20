e1f7a745adb3d500bfcd24353186883eff7e8169
Buster Neece <buster@busterneece.com>
2024-01-20 10:38:22 -0600

    Use common FullHeightCard component.

--- frontend/src/components/Public/Schedule.vue
+++ frontend/src/components/Public/Schedule.vue
@@ -1,41 +1,30 @@
 <template>
-    <section
-        id="content"
-        class="full-height-wrapper"
-        role="main"
-    >
-        <div class="container">
-            <div class="card">
-                <div class="card-header text-bg-primary">
-                    <div class="d-flex align-items-center">
-                        <div class="flex-shrink">
-                            <h2 class="card-title py-2">
-                                <template v-if="stationName">
-                                    {{ stationName }}
-                                </template>
-                                <template v-else>
-                                    {{ $gettext('Schedule') }}
-                                </template>
-                            </h2>
-                        </div>
-                    </div>
-                </div>
-
-                <div id="station-schedule-calendar">
-                    <schedule
-                        ref="schedule"
-                        :timezone="stationTimeZone"
-                        :schedule-url="scheduleUrl"
-                        :station-time-zone="stationTimeZone"
-                    />
-                </div>
+    <full-height-card>
+        <template #title>
+            <template v-if="stationName">
+                {{ stationName }}
+            </template>
+            <template v-else>
+                {{ $gettext('Schedule') }}
+            </template>
+        </template>
+        
+        <template #default>
+            <div id="station-schedule-calendar">
+                <schedule
+                    ref="schedule"
+                    :timezone="stationTimeZone"
+                    :schedule-url="scheduleUrl"
+                    :station-time-zone="stationTimeZone"
+                />
             </div>
-        </div>
-    </section>
+        </template>
+    </full-height-card>
 </template>
 
 <script setup lang="ts">
 import Schedule from '~/components/Common/ScheduleView.vue';
+import FullHeightCard from "~/components/Public/FullHeightCard.vue";
 
 const props = defineProps({
     scheduleUrl: {

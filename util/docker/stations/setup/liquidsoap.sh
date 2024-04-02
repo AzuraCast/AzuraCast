5fe95f4dce2c90fc9ae6430ef2cf65392ef5500d
RM-FM <40538914+RM-FM@users.noreply.github.com>
2024-04-02 17:53:04 +0200

    #6252 -- Update LS rolling release for AutoCue improvements (#7039)
    * #6252 -- Update to LS rolling release for AutoCue improvements
    
    https://github.com/savonet/liquidsoap/pull/3811
    
    * Update liquidsoap.sh
    
    ---------
    
    Co-authored-by: Buster Neece <buster@busterneece.com>

--- util/docker/stations/setup/liquidsoap.sh
+++ util/docker/stations/setup/liquidsoap.sh
@@ -20,7 +20,7 @@ if [[ "$(uname -m)" = "aarch64" ]]; then
 fi
 
 # wget -O /tmp/liquidsoap.deb "https://github.com/savonet/liquidsoap/releases/download/v2.2.4/liquidsoap_2.2.4-debian-bookworm-2_${ARCHITECTURE}.deb"
-wget -O /tmp/liquidsoap.deb "https://github.com/savonet/liquidsoap-release-assets/releases/download/rolling-release-v2.2.x/liquidsoap-1c0baea_2.2.5-debian-bookworm-1_${ARCHITECTURE}.deb"
+wget -O /tmp/liquidsoap.deb "https://github.com/savonet/liquidsoap-release-assets/releases/download/rolling-release-v2.2.x/liquidsoap-cadd055_2.2.5-debian-bookworm-1_${ARCHITECTURE}.deb"
 
 dpkg -i /tmp/liquidsoap.deb
 apt-get install -y -f --no-install-recommends

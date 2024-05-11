9d75ca5b8bbed9b2eaa6e45c2228c7b89b45bb1a
Buster Neece <buster@busterneece.com>
2024-05-11 07:51:05 -0500

    PHPCS fixes.

--- src/Entity/Migration/Version20240511123636.php
+++ src/Entity/Migration/Version20240511123636.php
@@ -22,5 +22,4 @@ final class Version20240511123636 extends AbstractMigration
     {
         $this->addSql('CREATE TABLE cache_items (item_id VARBINARY(255) NOT NULL, item_data MEDIUMBLOB NOT NULL, item_lifetime INT UNSIGNED DEFAULT NULL, item_time INT UNSIGNED NOT NULL, PRIMARY KEY(item_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_general_ci` ENGINE = InnoDB COMMENT = \'\' ');
     }
-
 }

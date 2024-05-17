f108baa87daf51eef93edb4f67d6e5d99c559f56
Buster Neece <buster@busterneece.com>
2024-05-17 06:29:27 -0500

    Refactor request submission and move exceptions into Http exception.

--- src/Entity/Repository/StationRequestRepository.php
+++ src/Entity/Repository/StationRequestRepository.php
@@ -8,11 +8,7 @@ use App\Entity\Api\StationPlaylistQueue;
 use App\Entity\Station;
 use App\Entity\StationMedia;
 use App\Entity\StationRequest;
-use App\Enums\StationFeatures;
-use App\Exception;
 use App\Radio\AutoDJ;
-use App\Radio\Frontend\Blocklist\BlocklistParser;
-use App\Service\DeviceDetector;
 use Carbon\CarbonImmutable;
 use Carbon\CarbonInterface;
 use Exception as PhpException;
@@ -25,9 +21,6 @@ final class StationRequestRepository extends AbstractStationBasedRepository
     protected string $entityClass = StationRequest::class;
 
     public function __construct(
-        private readonly StationMediaRepository $mediaRepo,
-        private readonly DeviceDetector $deviceDetector,
-        private readonly BlocklistParser $blocklistParser,
         private readonly AutoDJ\DuplicatePrevention $duplicatePrevention,
     ) {
     }
@@ -55,91 +48,10 @@ final class StationRequestRepository extends AbstractStationBasedRepository
             ->execute();
     }
 
-    public function submit(
-        Station $station,
-        string $trackId,
-        bool $isAuthenticated,
-        string $ip,
-        string $userAgent
-    ): int {
-        // Verify that the station supports requests.
-        StationFeatures::Requests->assertSupportedForStation($station);
-
-        // Forbid web crawlers from using this feature.
-        $dd = $this->deviceDetector->parse($userAgent);
-
-        if ($dd->isBot) {
-            throw Exception\CannotCompleteActionException::submitRequest(
-                __('Search engine crawlers are not permitted to use this feature.')
-            );
-        }
-
-        // Check frontend blocklist and apply it to requests.
-        if (!$this->blocklistParser->isAllowed($station, $ip, $userAgent)) {
-            throw Exception\CannotCompleteActionException::submitRequest(
-                __('You are not permitted to submit requests.')
-            );
-        }
-
-        // Verify that Track ID exists with station.
-        $mediaItem = $this->mediaRepo->requireByUniqueId($trackId, $station);
-
-        if (!$mediaItem->isRequestable()) {
-            throw Exception\CannotCompleteActionException::submitRequest(
-                __('This track is not requestable.')
-            );
-        }
-
-        // Check if the song is already enqueued as a request.
-        $this->checkPendingRequest($mediaItem, $station);
-
-        // Check the most recent song history.
-        $this->checkRecentPlay($mediaItem, $station);
-
-        if (!$isAuthenticated) {
-            // Check for any request (on any station) within the last $threshold_seconds.
-            $thresholdMins = $station->getRequestDelay() ?? 5;
-            $thresholdSeconds = $thresholdMins * 60;
-
-            // Always have a minimum threshold to avoid flooding.
-            if ($thresholdSeconds < 60) {
-                $thresholdSeconds = 15;
-            }
-
-            $recentRequests = (int)$this->em->createQuery(
-                <<<'DQL'
-                    SELECT COUNT(sr.id) FROM App\Entity\StationRequest sr
-                    WHERE sr.ip = :user_ip
-                    AND sr.timestamp >= :threshold
-                DQL
-            )->setParameter('user_ip', $ip)
-                ->setParameter('threshold', time() - $thresholdSeconds)
-                ->getSingleScalarResult();
-
-            if ($recentRequests > 0) {
-                throw Exception\CannotCompleteActionException::submitRequest(
-                    __('You have submitted a request too recently! Please wait before submitting another one.')
-                );
-            }
-        }
-
-        // Save request locally.
-        $record = new StationRequest($station, $mediaItem, $ip);
-        $this->em->persist($record);
-        $this->em->flush();
-
-        return $record->getIdRequired();
-    }
-
     /**
      * Check if the song is already enqueued as a request.
-     *
-     * @param StationMedia $media
-     * @param Station $station
-     *
-     * @throws Exception
      */
-    public function checkPendingRequest(StationMedia $media, Station $station): bool
+    public function isTrackPending(StationMedia $media, Station $station): bool
     {
         $pendingRequestThreshold = time() - (60 * 10);
 
@@ -159,16 +71,10 @@ final class StationRequestRepository extends AbstractStationBasedRepository
                 ->setMaxResults(1)
                 ->getSingleScalarResult();
         } catch (PhpException) {
-            return true;
+            return false;
         }
 
-        if ($pendingRequest > 0) {
-            throw Exception\CannotCompleteActionException::submitRequest(
-                __('This song was already requested and will play soon.')
-            );
-        }
-
-        return true;
+        return ($pendingRequest > 0);
     }
 
     public function getNextPlayableRequest(
@@ -191,13 +97,7 @@ final class StationRequestRepository extends AbstractStationBasedRepository
 
         foreach ($requests as $request) {
             /** @var StationRequest $request */
-            if ($request->shouldPlayNow($now)) {
-                try {
-                    $this->checkRecentPlay($request->getTrack(), $station);
-                } catch (PhpException) {
-                    continue;
-                }
-
+            if ($request->shouldPlayNow($now) && !$this->hasPlayedRecently($request->getTrack(), $station)) {
                 return $request;
             }
         }
@@ -207,18 +107,13 @@ final class StationRequestRepository extends AbstractStationBasedRepository
 
     /**
      * Check the most recent song history.
-     *
-     * @param StationMedia $media
-     * @param Station $station
-     *
-     * @throws Exception
      */
-    public function checkRecentPlay(StationMedia $media, Station $station): bool
+    public function hasPlayedRecently(StationMedia $media, Station $station): bool
     {
         $lastPlayThresholdMins = ($station->getRequestThreshold() ?? 15);
 
         if (0 === $lastPlayThresholdMins) {
-            return true;
+            return false;
         }
 
         $lastPlayThreshold = time() - ($lastPlayThresholdMins * 60);
@@ -240,14 +135,6 @@ final class StationRequestRepository extends AbstractStationBasedRepository
         $eligibleTrack->title = $media->getTitle() ?? '';
         $eligibleTrack->artist = $media->getArtist() ?? '';
 
-        $isDuplicate = (null === $this->duplicatePrevention->getDistinctTrack([$eligibleTrack], $recentTracks));
-
-        if ($isDuplicate) {
-            throw Exception\CannotCompleteActionException::submitRequest(
-                __('This song or artist has been played too recently. Wait a while before requesting it again.')
-            );
-        }
-
-        return true;
+        return (null === $this->duplicatePrevention->getDistinctTrack([$eligibleTrack], $recentTracks));
     }
 }

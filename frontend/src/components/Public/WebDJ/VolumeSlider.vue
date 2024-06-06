938db4717e50dc66d56c81a8f5da19f6cd87bc73
Buster Neece <buster@busterneece.com>
2024-06-06 01:08:44 -0500

    Update WebDJ to use common volume control.

--- frontend/src/components/Public/WebDJ/VolumeSlider.vue
+++ frontend/src/components/Public/WebDJ/VolumeSlider.vue
@@ -1,7 +1,12 @@
 <template>
     <div class="d-flex flex-row align-items-center">
-        <div class="flex-shrink-0">
-            <icon :icon="IconVolumeOff" />
+        <div class="flex-shrink-0 me-2">
+            <mute-button
+                class="p-0"
+                :volume="volume"
+                :is-muted="isMuted"
+                @toggle-mute="toggleMute"
+            />
         </div>
         <div class="flex-fill px-2">
             <input
@@ -10,21 +15,17 @@
                 min="0"
                 max="100"
                 class="form-range slider"
-                style="height: 10px; width: 100px;"
+                style="height: 10px; width: 125px;"
                 @click.right.prevent="reset"
             >
-        </div>
-        <div class="flex-shrink-0">
-            <icon :icon="IconVolumeUp" />
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
-import Icon from "~/components/Common/Icon.vue";
-import {onMounted, ref} from "vue";
+import {computed, onMounted, ref} from "vue";
 import {useVModel} from "@vueuse/core";
-import {IconVolumeOff, IconVolumeUp} from "~/components/Common/icons";
+import MuteButton from "~/components/Common/MuteButton.vue";
 
 const props = defineProps({
     modelValue: {
@@ -34,13 +35,28 @@ const props = defineProps({
 });
 
 const emit = defineEmits(['update:modelValue']);
+const volume = useVModel(props, 'modelValue', emit);
 
 const initial = ref(75);
+const preMute = ref(75);
+
 onMounted(() => {
     initial.value = props.modelValue;
+    preMute.value = props.modelValue;
 });
 
-const volume = useVModel(props, 'modelValue', emit);
+const isMuted = computed(() => {
+    return volume.value === 0;
+});
+
+const toggleMute = () => {
+    if (isMuted.value) {
+        volume.value = preMute.value;
+    } else {
+        preMute.value = volume.value;
+        volume.value = 0;
+    }
+}
 
 const reset = () => {
     volume.value = initial.value;

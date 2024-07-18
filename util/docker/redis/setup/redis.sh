67d2c0349b1c7a5b37e3fa49e5ca08104f1d4ad8
Buster Neece <buster@busterneece.com>
2024-07-18 01:17:53 -0500

    Switch from Redis to Redict via backports.

--- util/docker/redis/setup/redis.sh
+++ util/docker/redis/setup/redis.sh
@@ -2,14 +2,10 @@
 set -e
 set -x
 
-curl -fsSL https://packages.redis.io/gpg | sudo gpg --dearmor -o /usr/share/keyrings/redis-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/redis.list
+apt-get install -y --no-install-recommends redict-server/bookworm-backports redict-tools/bookworm-backports
 
-apt-get update
-apt-get install -y --no-install-recommends redis-server=6:7.2.4-1rl1~bookworm1 redis-tools=6:7.2.4-1rl1~bookworm1
-
-cp /bd_build/redis/redis/redis.conf /etc/redis/redis.conf
-chown redis:redis /etc/redis/redis.conf
+cp /bd_build/redis/redis/redis.conf /etc/redict/redict.conf
+chown redict:redict /etc/redict/redict.conf
 
 mkdir -p /run/redis
-chown redis:redis /run/redis
+chown redict:redict /run/redis

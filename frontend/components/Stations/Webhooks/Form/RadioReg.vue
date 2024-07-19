9161c7bd461a5ebd65a3b2c3c205432e7e2a48f8
Mason Daugherty <mdaugher@bowdoin.edu>
2024-07-19 02:36:51 -0400

    chore: Add descriptions for RadioReg Webhook URL and API Key fields

--- frontend/components/Stations/Webhooks/Form/RadioReg.vue
+++ frontend/components/Stations/Webhooks/Form/RadioReg.vue
@@ -9,6 +9,7 @@
                 class="col-md-12"
                 :field="v$.config.webhookurl"
                 :label="$gettext('RadioReg Webhook URL')"
+                :description="$gettext('Found under the settings page for the corresponding RadioReg station.')"
             />
 
             <form-group-field
@@ -16,6 +17,7 @@
                 class="col-md-6"
                 :field="v$.config.apikey"
                 :label="$gettext('RadioReg Organization API Key')"
+                :description="$gettext('An API token is issued on a per-organization basis and are found on the org. settings page.')"
             />
         </div>
     </tab>

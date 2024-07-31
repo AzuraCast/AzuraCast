1c399f8fe577752aa7f676b52198fea50553cd42
Buster Neece <buster@busterneece.com>
2024-07-31 14:56:02 -0500

    Revert "Merge branch 'dev-ls2.3.x' of github.com:AzuraCast/AzuraCast"
    This reverts commit 4d2f922d59fa3b8de61e23aaaeab7e88e821ec27, reversing
    changes made to 8515ee972da6353aeb3a6d1e0de8be1a01f70522.

--- util/docker/stations/setup/liquidsoap.sh
+++ util/docker/stations/setup/liquidsoap.sh
@@ -6,8 +6,8 @@ set -x
 apt-get install -y --no-install-recommends \
     libao4 libfaad2 libfdk-aac2 libgd3 liblo7 libmad0 libmagic1 libportaudio2 \
     libsdl2-image-2.0-0 libsdl2-ttf-2.0-0 libsoundtouch1 libxpm4 \
-    libasound2 libavcodec60 libavdevice60 libavfilter9 libavformat60 libavutil58 \
-    libpulse0 libsamplerate0 libswresample4 libswscale7 libtag1v5 \
+    libasound2 libavcodec59 libavdevice59 libavfilter8 libavformat59 libavutil57 \
+    libpulse0 libsamplerate0 libswresample4 libswscale6 libtag1v5 \
     libsrt1.5-openssl bubblewrap ffmpeg liblilv-0-0 libjemalloc2 libpcre3
 
 # Audio Post-processing
@@ -19,8 +19,8 @@ if [[ "$(uname -m)" = "aarch64" ]]; then
     ARCHITECTURE=arm64
 fi
 
-wget -O /tmp/liquidsoap.deb "https://github.com/savonet/liquidsoap-release-assets/releases/download/rolling-release-v2.3.x/liquidsoap-10453cf_2.3.0-debian-bookworm-1_${ARCHITECTURE}.deb"
-# wget -O /tmp/liquidsoap.deb "https://github.com/savonet/liquidsoap/releases/download/v2.2.5/liquidsoap_2.2.5-debian-bookworm-1_${ARCHITECTURE}.deb"
+# wget -O /tmp/liquidsoap.deb "https://github.com/savonet/liquidsoap-release-assets/releases/download/rolling-release-v2.2.x/liquidsoap-cadd055_2.2.5-debian-bookworm-1_${ARCHITECTURE}.deb"
+wget -O /tmp/liquidsoap.deb "https://github.com/savonet/liquidsoap/releases/download/v2.2.5/liquidsoap_2.2.5-debian-bookworm-1_${ARCHITECTURE}.deb"
 
 dpkg -i /tmp/liquidsoap.deb
 apt-get install -y -f --no-install-recommends

0fb2882fc570640b5017e5ed5b64e73c24545799
Buster Neece <buster@busterneece.com>
2025-01-06 09:11:46 -0600

    #7656 -- Set permissions on base persist dirs in case they're newly created.

--- util/docker/web/startup_scripts/03_persist_dir.sh
+++ util/docker/web/startup_scripts/03_persist_dir.sh
@@ -4,9 +4,13 @@ export DEBIAN_FRONTEND=noninteractive
 
 echo "Creating persist directories..."
 
-mkdir -p /var/azuracast/storage/uploads \
+mkdir -p \
+  /var/azuracast/storage/acme \
+  /var/azuracast/storage/geoip \
+  /var/azuracast/storage/rsas \
+  /var/azuracast/storage/sftpgo \
   /var/azuracast/storage/shoutcast2 \
   /var/azuracast/storage/stereo_tool \
-  /var/azuracast/storage/geoip \
-  /var/azuracast/storage/sftpgo \
-  /var/azuracast/storage/acme
+  /var/azuracast/storage/uploads
+
+chown azuracast:azuracast /var/azuracast/storage/* || true

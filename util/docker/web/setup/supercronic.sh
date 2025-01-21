23f508129c4eee6c5b6e6773ac5004ed6c1c8f5c
Buster Neece <buster@busterneece.com>
2025-01-21 05:34:07 -0600

    Update Centrifugo; switch Golang dependencies to build from their individual binary releases.

---
+++ util/docker/web/setup/supercronic.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+set -x
+
+export SUPERCRONIC_VERSION=0.2.33
+
+# Per-architecture LS installs
+ARCHITECTURE=amd64
+if [[ "$(uname -m)" = "aarch64" ]]; then
+    ARCHITECTURE=arm64
+fi
+
+wget -O /usr/local/bin/supercronic "https://github.com/aptible/supercronic/releases/download/v${SUPERCRONIC_VERSION}/supercronic-linux-${ARCHITECTURE}"
+chmod a+x /usr/local/bin/supercronic

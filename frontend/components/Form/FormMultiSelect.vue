fad067e3480512accb997a173edc31e701dc4bc6
Buster Neece <buster@busterneece.com>
2025-01-28 12:02:14 -0600

    Move form select/multiple-select to their own components so they have different default values that suit their respective types.

---
+++ frontend/components/Form/FormMultiSelect.vue
@@ -0,0 +1,23 @@
+<template>
+    <select
+        v-model="model"
+        class="form-select"
+        multiple
+    >
+        <select-options :options="options"/>
+    </select>
+</template>
+
+<script setup lang="ts" generic="T = ModelFormField">
+import {NestedFormOptionInput} from "~/functions/objectToFormOptions.ts";
+import SelectOptions from "~/components/Form/SelectOptions.vue";
+import {ModelFormField} from "~/components/Form/useFormField.ts";
+
+defineProps<{
+    options: NestedFormOptionInput
+}>();
+
+const model = defineModel<T>({
+    default: () => [],
+});
+</script>

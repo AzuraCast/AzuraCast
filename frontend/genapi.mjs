01155f93a4b4267b95fe8c28966e354a74b62925
Buster Neece <buster@busterneece.com>
2025-02-26 12:31:49 -0600

    Move API generation to its own file.

---
+++ frontend/genapi.mjs
@@ -0,0 +1,12 @@
+import path from "node:path";
+import {generateApi} from "swagger-typescript-api";
+
+const __dirname = import.meta.dirname;
+
+generateApi({
+    name: "ApiInterfaces.ts",
+    output: path.resolve(__dirname, "./entities"),
+    url: "http://localhost/api/openapi.yml",
+    generateClient: false,
+    generateUnionEnums: false,
+});

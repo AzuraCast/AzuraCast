daf05055ed1a894d7807ce6c32ff9444113653ed
Vaalyn <vaalyndev@gmail.com>
2025-06-25 12:05:33 +0200

    Fix writing metadata for aac files due to bug in upstream library

--- backend/src/Media/MimeTypeExtensionMap.php
+++ backend/src/Media/MimeTypeExtensionMap.php
@@ -11,12 +11,15 @@ final class MimeTypeExtensionMap extends GeneratedExtensionToMimeTypeMap
     public const array ADDED_MIME_TYPES = [
         'mod' => 'audio/x-mod',
         'stm' => 'audio/x-mod',
+        // Temporary Bugfix for incorrectly mapped aac mime type in library
+        // @see https://github.com/thephpleague/mime-type-detection/pull/40
+        'aac' => 'audio/aac',
     ];
 
     public function lookupMimeType(string $extension): ?string
     {
-        return parent::lookupMimeType($extension)
-            ?? self::ADDED_MIME_TYPES[$extension]
+        return self::ADDED_MIME_TYPES[$extension]
+            ?? parent::lookupMimeType($extension)
             ?? null;
     }
 }

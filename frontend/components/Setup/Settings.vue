0c332d9cbaa062417754986bbb7023a73cb64b43
Buster Neece <buster@busterneece.com>
2025-08-24 11:56:01 -0500

    Add container limits to public pages.

--- frontend/components/Setup/Settings.vue
+++ frontend/components/Setup/Settings.vue
@@ -4,29 +4,27 @@
         class="full-height-wrapper"
         role="main"
     >
-        <div class="container" id="content">
-            <admin-settings
-                v-bind="props"
-                @saved="onSaved"
-            >
-                <template #preCard>
-                    <setup-step :step="3"/>
-                </template>
-                <template #cardTitle>
-                    {{ $gettext('Customize AzuraCast Settings') }}
-                </template>
-                <template #cardUpper>
-                    <info-card>
-                        {{
-                            $gettext('Complete the setup process by providing some information about your broadcast environment. These settings can be changed later from the administration panel.')
-                        }}
-                    </info-card>
-                </template>
-                <template #submitButtonName>
-                    {{ $gettext('Save and Continue') }}
-                </template>
-            </admin-settings>
-        </div>
+        <admin-settings
+            v-bind="props"
+            @saved="onSaved"
+        >
+            <template #preCard>
+                <setup-step :step="3"/>
+            </template>
+            <template #cardTitle>
+                {{ $gettext('Customize AzuraCast Settings') }}
+            </template>
+            <template #cardUpper>
+                <info-card>
+                    {{
+                        $gettext('Complete the setup process by providing some information about your broadcast environment. These settings can be changed later from the administration panel.')
+                    }}
+                </info-card>
+            </template>
+            <template #submitButtonName>
+                {{ $gettext('Save and Continue') }}
+            </template>
+        </admin-settings>
     </section>
 </template>
 
--- frontend/components/Setup/Station.vue
+++ frontend/components/Setup/Station.vue
@@ -6,42 +6,40 @@
         class="full-height-wrapper"
         role="main"
     >
-        <div class="container" id="content">
-            <section
-                class="card"
-                role="region"
-                aria-labelledby="hdr_new_station"
-            >
-                <div class="card-header text-bg-primary">
-                    <h3
-                        id="hdr_new_station"
-                        class="card-title"
-                    >
-                        {{ $gettext('Create a New Radio Station') }}
-                    </h3>
-                </div>
+        <section
+            class="card"
+            role="region"
+            aria-labelledby="hdr_new_station"
+        >
+            <div class="card-header text-bg-primary">
+                <h3
+                    id="hdr_new_station"
+                    class="card-title"
+                >
+                    {{ $gettext('Create a New Radio Station') }}
+                </h3>
+            </div>
 
-                <info-card>
-                    {{
-                        $gettext('Continue the setup process by creating your first radio station below. You can edit any of these details later.')
-                    }}
-                </info-card>
+            <info-card>
+                {{
+                    $gettext('Continue the setup process by creating your first radio station below. You can edit any of these details later.')
+                }}
+            </info-card>
 
-                <div class="card-body">
-                    <admin-stations-form
-                        v-bind="props"
-                        ref="$adminForm"
-                        :is-edit-mode="false"
-                        :create-url="createUrl"
-                        @submitted="onSubmitted"
-                    >
-                        <template #submitButtonText>
-                            {{ $gettext('Create and Continue') }}
-                        </template>
-                    </admin-stations-form>
-                </div>
-            </section>
-        </div>
+            <div class="card-body">
+                <admin-stations-form
+                    v-bind="props"
+                    ref="$adminForm"
+                    :is-edit-mode="false"
+                    :create-url="createUrl"
+                    @submitted="onSubmitted"
+                >
+                    <template #submitButtonText>
+                        {{ $gettext('Create and Continue') }}
+                    </template>
+                </admin-stations-form>
+            </div>
+        </section>
     </section>
 </template>
 

7c936e3674c230070d22bdf64447ed3db73ac336
Buster Neece <buster@busterneece.com>
2025-09-01 13:55:39 -0500

    Update to Liquidsoap 2.4.0 release.

--- util/docker/stations/setup/liquidsoap.sh
+++ util/docker/stations/setup/liquidsoap.sh
@@ -18,8 +18,8 @@ if [[ "$(uname -m)" = "aarch64" ]]; then
     ARCHITECTURE=arm64
 fi
 
-# wget -O /tmp/liquidsoap.deb "https://github.com/savonet/liquidsoap/releases/download/v2.3.2/liquidsoap_2.3.2-debian-trixie-2_${ARCHITECTURE}.deb"
-wget -O /tmp/liquidsoap.deb "https://github.com/savonet/liquidsoap/releases/download/rolling-release-v2.4.x/liquidsoap-a3e5bda_2.4.0-debian-trixie-ocaml4.14.2-1_${ARCHITECTURE}.deb"
+wget -O /tmp/liquidsoap.deb "https://github.com/savonet/liquidsoap/releases/download/v2.4.0/liquidsoap_2.4.0-debian-trixie-ocaml4.14.2-1_${ARCHITECTURE}.deb"
+# wget -O /tmp/liquidsoap.deb "https://github.com/savonet/liquidsoap/releases/download/rolling-release-v2.4.x/liquidsoap-a3e5bda_2.4.0-debian-trixie-ocaml4.14.2-1_${ARCHITECTURE}.deb"
 
 dpkg -i /tmp/liquidsoap.deb
 apt-get install -y -f --no-install-recommends
